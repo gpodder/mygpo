e5b93b2379e00c478e360751533bdccfddc1f106
Stefan Koegl <stefan@k-wh.at>
2009-11-03 07:43:56 +0000

    added simple home site, updated models

--- mygpo/api/models.py
+++ mygpo/api/models.py
@@ -26,6 +26,9 @@ class User(models.Model):
     password = models.CharField(max_length=200)
     email = models.EmailField()
     public_profile = models.BooleanField()
+
+    def __unicode__(self):
+        return self.username
     
     class Meta:
         db_table = 'user'
@@ -36,6 +39,9 @@ class Podcast(models.Model):
     description = models.TextField()
     link = models.URLField()
     last_update = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.title
     
     class Meta:
         db_table = 'podcast'
@@ -46,38 +52,54 @@ class Episode(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     link = models.URLField()
+
+    def __unicode__(self):
+        return self.title
     
     class Meta:
         db_table = 'episode'
 
 class EpisodeAction(models.Model):
-    db_table = 'episode_log'
-    
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, primary_key=True)
     episode = models.ForeignKey(Episode)
     action = models.CharField(max_length=10, choices=EPISODE_ACTION_TYPES)
     timestamp = models.DateTimeField()
+
+    def __unicode__(self):
+        return '%s %s %s' % self.user, self.action, self.episode
+
+    class Meta:
+        db_table = 'episode_log'
 
 class Device(models.Model):
     user = models.ForeignKey(User)
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=10, choices=DEVICE_TYPES)
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.name, self.type)
     
     class Meta:
         db_table = 'device'
 
 class Subscription(models.Model):
-    user = models.ForeignKey(User)
+    device = models.ForeignKey(Device, primary_key=True)
     podcast = models.ForeignKey(Podcast)
+
+    def __unicode__(self):
+        return '(%s, %s)' % (self.user, self.podcast)
     
     class Meta:
         db_table = 'current_subscription'
 
 class SubscriptionAction(models.Model):
-    device = models.ForeignKey(Device)
+    device = models.ForeignKey(Device, primary_key=True)
     podcast = models.ForeignKey(Podcast)
     action = models.CharField(max_length=12, choices=SUBSCRIPTION_ACTION_TYPES)
     timestamp = models.DateTimeField()
+
+    def __unicode__(self):
+        return '%s %s %s' % (self.device, self.action, self.podcast)
     
     class Meta:
         db_table = 'subscription_log'
--- mygpo/web/views.py
+++ mygpo/web/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from mygpo.api.models import Podcast
+
+def home(request):
+    podcasts = Podcast.objects.count()
+    return render_to_response('home.html', {
+            'podcast_count': podcasts
+        })
+
