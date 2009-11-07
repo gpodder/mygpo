8d4303eaf54d0b29da19459e1101beff6e0779b4
Stefan Koegl <stefan@skoegl.net>
2009-11-07 21:27:55 +0100

    removing db password from settings

--- mygpo/settings.py
+++ mygpo/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'mygpo'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'mygpo'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'mygpo'         # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
