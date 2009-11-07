8b16b50e0434a27ce5756cb3949584747d206e57
Stefan Koegl <stefan@skoegl.net>
2009-11-07 21:29:25 +0100

    partially implemented legacy upload

--- mygpo/api/legacy.py
+++ mygpo/api/legacy.py
@@ -1,20 +1,55 @@
-# Create your views here.
+from django.http import HttpResponse
+from mygpo.api.opml import Importer
+from mygpo.api.models import Subscription, Podcast, UserAccount
 
 def upload(request):
+    emailaddr = request.GET['username']
     action    = request.GET['action']
     protocol  = request.GET['protocol']
     opml_file = request.FILES['opml']
     opml      = opml_file.read()
     
-    if !auth(request):
-        #@AUTHFAIL
-    
-    #read podcasts
+    if (not auth(request)):
+        return HttpResponse('@AUTHFAIL')
 
+    try:
+        existing = Subscription.objects.get(user__email__exact=emailaddr)
+    except Subscription.DoesNotExist:
+        existing = []
+
+    print existing
+
+    existing_urls = [e.podcast.url for e in existing]
+
+    print existing_urls
+    
+    i = Importer(content=request.raw_post_data)
+    podcasts = i.items
+
+    new = [item for item in i.items if item['url'] not in existing_urls]
+    rem = [e.podcast for e in existing if e.podcast.url not in i.items]
+
+    print new
+    print rem
+
+    for n in new:
+        try:
+            p = Podcast.objects.get(url__exact=n['url'])
+        except Podcast.DoesNotExist:
+            p = Podcast(url=n['url'])
+            p.save()
+        s = SubscriptionAction(podcast=p,action='subscribe', timestamp=now())
+        s.save()
+
+    for p in rem:
+        s = SubscriptionAction(podcast=p, action='unsubscribe', timestamp=now())
+        s.save()
+
+    return HttpResponse('@SUCCESS')
 
 def getlist(request):
-    if !auth(request):
-        #@AUTHFAIL
+    if (not auth(request)):
+        return HttpResponse('@AUTHFAIL')
 
     # build and send list
 
@@ -23,12 +58,13 @@ def auth(request):
     emailaddr = request.GET['username']
     password  = request.GET['password']
 
-    user = UserAccount.objects.get(email__exact=emailaddr)
-    if !user:
-        return false
+    try:
+        user = UserAccount.objects.get(email__exact=emailaddr)
+    except UserAccount.DoesNotExist:
+        return False
 
-    if !user.check_password(password):
-        return false
+    if not user.check_password(password):
+        return False
     
     return user
 
