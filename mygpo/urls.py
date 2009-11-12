b2c4a0bf7604b44d3122294508f7895133415668
Stefan Koegl <stefan@skoegl.net>
2009-11-12 17:46:50 +0100

    removed default_device from simple api

--- mygpo/urls.py
+++ mygpo/urls.py
@@ -13,7 +13,6 @@ urlpatterns = patterns('',
     (r'^upload$', 'mygpo.api.legacy.upload'),
     (r'^getlist$', 'mygpo.api.legacy.getlist'),
  
-    (r'^subscriptions/(?P<username>\w+)/default.(?P<format>\w+)', 'mygpo.api.simple.all_subscriptions'),
     (r'^subscriptions/(?P<username>\w+)/(?P<device>\w+).(?P<format>\w+)', 'mygpo.api.simple.device_subscription'),
     
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
