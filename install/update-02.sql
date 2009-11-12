2ab27db8bae964b9b9b313da5122f25604ae50ba
Stefan Koegl <stefan@skoegl.net>
2009-11-12 17:30:33 +0100

    fixed typos in update-02.sql

--- install/update-02.sql
+++ install/update-02.sql
@@ -4,10 +4,10 @@ ALTER TABLE podcast ADD COLUMN logo_url VARCHAR(1000);
 
 -- Bug 649 --
 CREATE TABLE `sync_group` (
-    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `user_id` integer NOT NULL
-)
+);
 
 ALTER TABLE user DROP COLUMN default_device_id;
-ALTER TABLE device ADD COLUMN sync_group_id REFERENCES sync_group(id);
+ALTER TABLE device ADD COLUMN sync_group_id INT REFERENCES sync_group(id);
 ALTER TABLE device ADD COLUMN `uid` varchar(50) NOT NULL;
