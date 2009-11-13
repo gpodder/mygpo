470642e4e24db84b7402ce09e54ebcf86dbd8b51
Stefan Koegl <stefan@skoegl.net>
2009-11-13 11:34:47 +0100

    included file formats in simple api urls

--- mygpo/api/simple.py
+++ mygpo/api/simple.py
@@ -2,6 +2,6 @@ from mygpo.api.basic_auth import require_valid_user
 from django.http import HttpResponse
 
 @require_valid_user()
-def device_subscription(request, username, device, format):
+def subscriptions(request, username, device, format):
     return HttpResponse()
 
--- mygpo/urls.py
+++ mygpo/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     (r'^upload$', 'mygpo.api.legacy.upload'),
     (r'^getlist$', 'mygpo.api.legacy.getlist'),
  
-    (r'^subscriptions/(?P<username>\w+)/(?P<device>\w+).(?P<format>\w+)', 'mygpo.api.simple.device_subscription'),
+    (r'^subscriptions/(?P<username>\w+)/(?P<device>\w+).(?P<format>(txt|opml|json))', 'mygpo.api.simple.subscriptions'),
     
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
