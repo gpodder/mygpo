a219f6afd36a2b22935618a6f4bf0f4e680fd0d2
Alexander Duml <alexander.duml@gmail.com>
2009-11-19 21:33:46 +0100

    modified:   simple.py

--- mygpo/api/simple.py
+++ mygpo/api/simple.py
@@ -1,6 +1,8 @@
 from mygpo.api.basic_auth import require_valid_user
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from mygpo.api.models import Device
+from mygpo.api.opml import Exporter
+from mygpo.api.json import JsonResponse
 
 @require_valid_user()
 def subscriptions(request, username, device_uid, format):
@@ -9,10 +11,11 @@ def subscriptions(request, username, device_uid, format):
         return HttpResponseForbidden()
 
     if request.method == 'GET':
-        return format_subscriptions(get_subscriptions(username, device), format)
+        return format_subscriptions(get_subscriptions(username, device_uid), format)
         
     elif request.method == 'PUT':
-        return set_subscriptions(device, parse_subscription(request.raw_post_data, format))
+	return request.raw_post_data
+        #return set_subscriptions(device_uid, parse_subscription(request.raw_post_data, format))
 
     else:
         return HttpResponseBadReqest()
@@ -23,19 +26,19 @@ def format_subscriptions(subscriptions, format):
         #return subscriptions formatted as txt
         urls = [p.url for p in subscriptions]
         s = "\n".join(urls)
-        return HttpRequest(s, mimetype='text/plain')
+        return HttpResponse(s, mimetype='text/plain')
 
     elif format == 'opml':
-        e = Exporter(subscriptions)
-        return HttpRequest(e.generate(), mimetype='text/xml')
+        return HttpResponse(Exporter().generate(subscriptions), mimetype='text/xml')
     
     elif format == 'json':
-        return JsonRequest(subscriptions)
+	urls = [p.url for p in subscriptions]
+        return JsonResponse(urls)
 
 def get_subscriptions(username, device_uid):
     #get and return subscription list from database (use backend to sync)
     d = Device.objects.get(uid=device_uid, user__username=username)
-    return [p.podcast for p in device.get_subscriptions()]
+    return [p.podcast for p in d.get_subscriptions()]
 
 def parse_subscription(raw_post_data, format):
     if format == 'txt':
@@ -51,6 +54,6 @@ def parse_subscription(raw_post_data, format):
 
     else: raise ValueError('unsupported format %s' % format)
 
-def set_subscriptions(device, subscriptions):
+def set_subscriptions(device_uid, subscriptions):
     # save subscriptions in database
     pass
