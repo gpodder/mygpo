7547b91528969900e5d2744393e02dba82664db7
Thomas Perl <thp@thpinfo.com>
2009-11-24 23:40:48 +0100

    Apply the GNU AGPLv3 to the code (bug 301)
    Added COPYING (AGPLv3) + AUTHORS files.

--- mygpo/api/opml.py
+++ mygpo/api/opml.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 #
+# This file is part of my.gpodder.org.
 #
+# my.gpodder.org is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# my.gpodder.org is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with my.gpodder.org. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 #
 #  based on: gPodder's opml.py (2007-08-19)
 #            libopmlreader.py (2006-06-13)
