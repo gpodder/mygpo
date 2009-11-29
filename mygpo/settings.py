505ff09204b0afb01c433cf0639bee17057f725a
Martin Wieser <wieser_martin@hotmail.de>
2009-11-29 10:44:00 +0100

    internationalisation

--- mygpo/settings.py
+++ mygpo/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'mygpo.urls'
