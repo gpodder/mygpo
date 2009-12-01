af4546c84a2f0b729fa2d248d053253a26452034
Armin Muellner <armin.muellner@gmail.com>
2009-12-01 14:50:23 +0100

    Started with subscription list

--- mygpo/web/views.py
+++ mygpo/web/views.py
@@ -19,15 +19,49 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.template import RequestContext
-from mygpo.api.models import Podcast
+from mygpo.api.models import Podcast, UserProfile, Episode, Device, EpisodeAction, SubscriptionAction
 
 def home(request):
        if request.user.is_authenticated():
-              return render_to_response('home-user.html', context_instance=RequestContext(request))
+              userid = UserProfile.objects.filter(user=request.user)[0].id
+              device = Device.objects.filter(user=userid)
+              device_ids = [d.id for d in device]
+              sublog = SubscriptionAction.objects.filter(device__in=device_ids)
+              sublog_podcastids = [s.podcast_id for s in sublog]
+              podcast = Podcast.objects.filter(id__in=sublog_podcastids)
+              podcast_titles = [p.title for p in podcast]
+              podcast_logourls = [p.logo_url for p in podcast]
+              podcast_ids = [p.id for p in podcast]
+                       
+              subscriptionlist = Ddict( dict )
+
+              for listindex, title in enumerate(podcast_titles):
+                    subscriptionlist[listindex]['logo'] = podcast_logourls[listindex]
+                    subscriptionlist[listindex]['title'] = title                     
+                    
+                    sl = SubscriptionAction.objects.filter(podcast=podcast_ids[listindex])
+                    sldev_ids = [s.device.id for s in sl]  
+                    dev = Device.objects.filter(id__in=sldev_ids)
+                    dev_names = [d.name for d in dev]
+                    subscriptionlist[listindex]['device'] = dev_names                          
+
+              return render_to_response('home-user.html', {
+                    'subscriptionlist': subscriptionlist,
+                    'test': subscriptionlist[0]['device']
+              }, context_instance=RequestContext(request))
 
        else:
               podcasts = Podcast.objects.count()
               return render_to_response('home.html', {
                     'podcast_count': podcasts
               })
+
+class Ddict(dict):
+    def __init__(self, default=None):
+        self.default = default
+
+    def __getitem__(self, key):
+        if not self.has_key(key):
+            self[key] = self.default()
+        return dict.__getitem__(self, key)
 
