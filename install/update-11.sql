4053081d9b1fe81d7f0ecdcca1ea1037c0af3484
Martin Wieser <wieser_martin@hotmail.de>
2010-01-14 16:52:48 +0100

    toplist coalesce

--- install/update-11.sql
+++ install/update-11.sql
@@ -168,7 +168,7 @@ BEGIN
             START TRANSACTION;
             DELETE FROM toplist_temp;
             INSERT INTO toplist_temp (SELECT a.podcast_id, COUNT(*) AS count_subscription, 
-                                       (select (id - (select min(id) from toplist) + 1) from toplist where podcast_id = a.podcast_id)
+                                       (select COALESCE((id - (select min(id) from toplist) + 1),0) from toplist where podcast_id = a.podcast_id)
                         FROM (SELECT DISTINCT podcast_id, user_id
                             FROM public_subscription) a
                         GROUP BY podcast_id);
