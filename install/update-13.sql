0f2e100f390d8a6509d1e08bdf45acbfa04ac772
Martin Wieser <wieser_martin@hotmail.de>
2010-01-27 19:19:14 +0100

    Bug 785 -  Deleting a Device
    Furthermore, subscriptions from such devices should not be included in
    current_subscriptions and other database views.

--- install/update-13.sql
+++ install/update-13.sql
@@ -1,3 +1,12 @@
 
 -- Bug 785 --
 ALTER TABLE device ADD COLUMN deleted tinyint(1) NOT NULL DEFAULT 0;
+DROP VIEW IF EXISTS current_subscription;
+
+CREATE VIEW current_subscription AS SELECT device_id, podcast_id, c.user_ptr_id AS user_id, a.timestamp as subscribed_since, sum(a.action) as summe
+    FROM (subscription_log a JOIN device b on a.device_id=b.id)
+        JOIN user c on b.user_id=c.user_ptr_id
+    WHERE b.deleted = 0
+    GROUP BY a.podcast_id, device_id
+    having summe>0;
+
