f1eff442e1b744a864e4a02256ab74884e7d4ace
Stefan Koegl <stefan@skoegl.net>
2010-01-29 00:13:52 +0100

    display podcast logos (everywhere)

--- mygpo/web/templatetags/podcasts.py
+++ mygpo/web/templatetags/podcasts.py
@@ -6,12 +6,12 @@ import hashlib
 register = template.Library()
 
 @register.filter
-def podcast_logo(podcast):
+def podcast_logo(podcast, size=32):
     if podcast.logo_url:
         sha = hashlib.sha1(podcast.logo_url).hexdigest()
-        s = '<img src="/media/logo/%s" alt="%s" />' % (sha, _('Logo'))
+        s = '<img src="/media/logo/%s" alt="%s" height="%s" width="%s" />' % (sha, _('Logo'), size, size)
     else:
-        s = '<img src="/media/logo/default.png" alt="%s" />' % _('No Logo available')
+        s = '<img src="/media/logo/default.png" alt="%s" height="%s" width="%s" />' % (_('No Logo available'), size, size)
 
     return mark_safe(s)
 
