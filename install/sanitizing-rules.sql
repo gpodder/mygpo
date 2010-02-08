16843d022b03f56ff7bfe60e078967b8ac583f3d
Stefan Koegl <stefan@skoegl.net>
2010-02-08 22:29:23 +0100

    add sanitizing rule to remove trailing slash from feedburner urls

--- install/sanitizing-rules.sql
+++ install/sanitizing-rules.sql
@@ -4,3 +4,5 @@ INSERT INTO `sanitizing_rules` VALUES (3,1,0,'^\\s+','',0,'Remove leading whites
 INSERT INTO `sanitizing_rules` VALUES (4,1,0,'\\s+$','',0,'Remove trailing whitespaces');
 INSERT INTO `sanitizing_rules` VALUES (5,1,0,'^[^(https?):].+','',100,'Empty any string that doesn\'t start with either http or https');
 INSERT INTO `sanitizing_rules` VALUES (6,1,0,'^https?://([0-9a-zA-z-\.]+\.)?gpodder.org.*','',100,'As gpodder.org doesn\'t host Podcasts, all URLs starting with this domain are considered invalid');
+INSERT INTO `sanitizing_rules` VALUES (7,1,0,'(?P<unchanged>feedburner\\.com.+)\\/$','\\g<unchanged>',2,'Feedburner URLs sometimes have a trailing slash, which can be removed safely');
+
