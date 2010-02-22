377d21dec435cdcf8eb2eb63b8b2b50a25b99dc4
Stefan Koegl <stefan@skoegl.net>
2010-02-22 17:10:47 +0100

    allways DELETE FROM sanitizing_rules before adding

--- install/sanitizing-rules.sql
+++ install/sanitizing-rules.sql
@@ -1,3 +1,4 @@
+DELETE FROM `sanitizing_rules`;
 INSERT INTO `sanitizing_rules` VALUES (1,1,1,'feeds2\\.feedburner\\.com','feeds.feedburner.com',1,'Rewriting for feedburner should happen as \"feeds2.feedburner.com\" -> \"feeds.feedburner.com\"');
 INSERT INTO `sanitizing_rules` VALUES (2,1,1,'(?P<unchanged>feedburner\\.com.+)\\?format=xml','\\g<unchanged>',2,'Feedburner URLs should have their \"?format=xml\" query string removed: \r\n\r\nhttp://feeds2.feedburner.com/linuxoutlaws?format=xml \r\nhttp://feeds.feedburner.com/linuxoutlaws \r\n');
 INSERT INTO `sanitizing_rules` VALUES (3,1,1,'^\\s+','',0,'Remove leading whitespaces');
