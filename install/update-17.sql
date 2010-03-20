694cf6fa8a04aded8f3ddd6539514179f60c7255
Stefan Koegl <stefan@skoegl.net>
2010-03-20 10:57:31 +0100

    create indexes for historic podcast/episode data

--- install/update-17.sql
+++ install/update-17.sql
@@ -18,3 +18,12 @@ CREATE TABLE `historic_episode_data` (
 ;
 ALTER TABLE `historic_episode_data` ADD CONSTRAINT `episode_id_refs_id_2f31345e` FOREIGN KEY (`episode_id`) REFERENCES `episode` (`id`);
 COMMIT;
+
+
+create index podcast_data_index on historic_podcast_data(podcast_id, date);
+create index podcast_data_podcast_index on historic_podcast_data(podcast_id);
+create index podcast_data_date_index on historic_podcast_data(date);
+create index episode_data_index on historic_episode_data(episode_id, date);
+create index episode_data_episode_index on historic_episode_data(episode_id);
+create index episode_data_date_index on historic_episode_data(date);
+
