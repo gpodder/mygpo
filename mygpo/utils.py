f3a5bc3d18ebd7b7fe4c234ddda5c3511b9a7981
Stefan Koegl <stefan@skoegl.net>
2010-03-21 17:50:12 +0100

    add parse_time to mygpo.utils, use in feed-downloader

--- mygpo/utils.py
+++ mygpo/utils.py
@@ -15,11 +15,26 @@
 # along with my.gpodder.org. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
+import time
 
 def daterange(from_date, to_date=datetime.now()):
     while from_date <= to_date:
         yield from_date
         from_date = from_date + timedelta(days=1)
     return
+
+
+def parse_time(str):
+    if not str:
+        raise ValueError('can\'t parse empty string')
+
+    for format in ('%H:%M:%S', '%M:%S'):
+        try:
+            t = time.strptime(str, format)
+            return t.tm_hour * 60*60 + t.tm_min * 60 + t.tm_sec
+        except ValueError, e:
+            continue
+
+    return int(str)
 
