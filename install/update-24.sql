daf9d6a94e9298eba26f54b980832b755145b9a8
Stefan Koegl <stefan@skoegl.net>
2010-04-08 21:12:16 +0200

    convert db, tables to utf8 (bug 972)

---
+++ install/update-24.sql
@@ -0,0 +1,6 @@
+alter database mygpo_prod default character set utf8 collate utf8_general_ci;
+alter table episode convert to character set utf8 collate utf8_general_ci;
+alter table chapters convert to character set utf8 collate utf8_general_ci;
+alter table podcast convert to character set utf8 collate utf8_general_ci;
+alter table device convert to character set utf8 collate utf8_general_ci;
+
