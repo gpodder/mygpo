b4cdd466e2cca37dcd6ce855e60ccaa8a0d99b17
Stefan Koegl <stefan@skoegl.net>
2010-04-20 15:12:52 +0200

    add option --preserve-oldplace to update-toplist

--- mygpo/data/management/commands/update-toplist.py
+++ mygpo/data/management/commands/update-toplist.py
@@ -1,18 +1,30 @@
 from django.core.management.base import BaseCommand
+from optparse import make_option
 from mygpo.api.models import Podcast, PodcastGroup, ToplistEntry, Subscription
 from django.db.models import Count
 
 class Command(BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+        make_option('--preserve-oldplace', action='store_true', dest='preserve_oldplace', default=False, help="Indicates that the Last (week's) position should not be updated."),
+    )
+
 
     def handle(self, *args, **options):
         old_podcasts = {}
         old_groups = {}
         n=1
         for t in ToplistEntry.objects.all().only('subscriptions', 'podcast', 'podcast_group').order_by('-subscriptions'):
-            if t.podcast:
-                old_podcasts[t.podcast.id] = n
+
+            if options.get('preserve_oldplace'):
+                oldplace = t.oldplace
             else:
-                old_groups[t.podcast_group.id] = n
+                oldplace = n
+
+            if t.podcast:
+                old_podcasts[t.podcast.id] = oldplace
+            else:
+                old_groups[t.podcast_group.id] = oldplace
             n += 1
 
         ToplistEntry.objects.all().delete()
