88c6cd497f22716dbaaec5e97220d69e68c75ef9
Stefan Koegl <stefan@skoegl.net>
2010-05-04 23:49:28 +0200

    optionally add google analytics code to base.html

--- mygpo/settings.py
+++ mygpo/settings.py
@@ -118,6 +118,16 @@ AUTHENTICATION_BACKENDS = (
     'mygpo.auth_backends.EmailAuthenticationBackend',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.contrib.messages.context_processors.messages",
+    "mygpo.web.googleanalytics.processor",
+)
+
+
 DEFAULT_FROM_EMAIL = 'mygpo@my.gpodder.org'
 
 AUTH_PROFILE_MODULE = "api.UserProfile"
