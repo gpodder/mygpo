c2eb40f3e97b2c10ff97a770b3197ad882767098
Stefan Koegl <stefan@skoegl.net>
2010-05-04 12:31:04 +0200

    avoid MultipleObjectsReturned exception for tags in feed downloader

--- mygpo/data/feeddownloader.py
+++ mygpo/data/feeddownloader.py
@@ -123,7 +123,8 @@ def update_feed_tags(podcast, tags):
 
     #create new found tags
     for tag in tags:
-        PodcastTag.objects.get_or_create(podcast=podcast, source=src, tag=tag)
+        if not PodcastTag.objects.filter(podcast=podcast, source=src, tag=tag).exists():
+            PodcastTag.objects.create(podcast=podcast, source=src, tag=tag)
 
 
 def get_episode_metadata(entry, url):
