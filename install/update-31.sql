b2e3fae643f80f123711977a319f026084052cdc
Stefan Koegl <stefan@skoegl.net>
2010-05-12 22:17:54 +0200

    update view episode_toplist to only include episodes with listeners

--- install/update-31.sql
+++ install/update-31.sql
@@ -1,2 +1,12 @@
 alter table podcast_tags modify tag varchar(100) character set utf8 collate utf8_general_ci not null;
 
+DROP VIEW IF EXISTS episode_toplist;
+
+CREATE VIEW episode_toplist AS
+    SELECT episode_id AS id, episode_id, count(episode_id) AS listeners
+    FROM recent_unique_plays
+    GROUP BY episode_id
+    HAVING listeners > 0
+    ORDER BY listeners;
+
+
