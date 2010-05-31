a58b0c065b8130779fbab789e7280d836d2d2639
Stefan Koegl <stefan@skoegl.net>
2010-05-31 21:42:13 +0200

    send only one play event per page load for youtube videos

--- htdocs/media/js/youtube-handler.js
+++ htdocs/media/js/youtube-handler.js
@@ -5,9 +5,10 @@ function embedYoutubeVideo(id, _username, _podcast_url, _episode_url) {
     var atts = { id: "ytplayer" };
 
     // store global variables for onytplayerStateChange
-    username = _username
-    podcast_url = _podcast_url
-    episode_url = _episode_url
+    username = _username;
+    podcast_url = _podcast_url;
+    episode_url = _episode_url;
+    already_played = false;
 
     swfobject.embedSWF("http://www.youtube.com/v/" + id + "?enablejsapi=1&playerapiid=ytplayer",
                        "ytapiplayer", "425", "356", "8", null, null, params, atts);
@@ -21,10 +22,11 @@ function onYouTubePlayerReady(playerId) {
 
 function onytplayerStateChange(newState) {
 
-    if (newState == 1)
+    if ((newState == 1) && !already_played)
     {
         var str = JSON.stringify([{"podcast": podcast_url, "episode": episode_url, "action": "play"}])
         $.post('/api/1/episodes/' + username + '.json', str);
+        already_played = true;
     }
 }
 
