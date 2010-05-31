12769a04eb04fb6965d4e146ff40bbafc8cf8dd6
Stefan Koegl <stefan@skoegl.net>
2010-05-31 20:01:59 +0200

    integrate youtube-videos on website (+ send play-events)

---
+++ htdocs/media/js/youtube-handler.js
@@ -0,0 +1,30 @@
+
+function embedYoutubeVideo(id, _username, _podcast_url, _episode_url) {
+
+    var params = { allowScriptAccess: "always" };
+    var atts = { id: "ytplayer" };
+
+    // store global variables for onytplayerStateChange
+    username = _username
+    podcast_url = _podcast_url
+    episode_url = _episode_url
+
+    swfobject.embedSWF("http://www.youtube.com/v/" + id + "?enablejsapi=1&playerapiid=ytplayer",
+                       "ytapiplayer", "425", "356", "8", null, null, params, atts);
+}
+
+
+function onYouTubePlayerReady(playerId) {
+    player = document.getElementById("ytplayer");
+    player.addEventListener("onStateChange", "onytplayerStateChange");
+}
+
+function onytplayerStateChange(newState) {
+
+    if (newState == 1)
+    {
+        var str = JSON.stringify([{"podcast": podcast_url, "episode": episode_url, "action": "play"}])
+        $.post('/api/1/episodes/' + username + '.json', str);
+    }
+}
+
---
+++ mygpo/web/templatetags/youtube.py
@@ -0,0 +1,22 @@
+from django import template
+from django.utils.safestring import mark_safe
+from mygpo.data import youtube
+
+register = template.Library()
+
+@register.filter
+def is_youtube_video(url):
+    return youtube.is_video_link(url)
+
+@register.filter
+def get_youtube_id(url):
+    return youtube.get_youtube_id(url)
+
+@register.filter
+def embed_youtube_video(episode, user):
+
+    s = """embedYoutubeVideo("%s", "%s", "%s", "%s");""" % \
+         (youtube.get_youtube_id(episode.link), user.username, episode.podcast.url, episode.url)
+
+    return mark_safe(s)
+
