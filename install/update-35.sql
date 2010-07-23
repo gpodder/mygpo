f67c817c22ace6da1a1c4bcaf4e3b78d641872e9
Stefan Koegl <stefan@skoegl.net>
2010-07-23 16:27:04 +0200

    update update-35.sql to reflect actually executed query

--- install/update-35.sql
+++ install/update-35.sql
@@ -4,5 +4,5 @@ create index mimetype on episode(mimetype);
 alter table podcast add column content_types varchar(30);
 create index content_types on podcast(content_types);
 
-alter table episode change timestamp timestamp datetime not null;
+alter table episode change timestamp timestamp datetime null;
 
