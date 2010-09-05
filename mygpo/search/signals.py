575e9ac522fe3d172c109755718ce3b18a9993d3
Stefan Koegl <stefan@skoegl.net>
2010-09-05 18:06:26 +0200

    remove SearchEntry objects for deleted podcast/podcast groups

--- mygpo/search/signals.py
+++ mygpo/search/signals.py
@@ -2,6 +2,15 @@ from django.db.models.signals import post_save, pre_delete
 from mygpo.api.models import Podcast, PodcastGroup
 from mygpo.search.util import podcast_entry, podcast_group_entry
 
+
+def remove_podcast_entry(sender, instance=False, **kwargs):
+    from mygpo.search.models import SearchEntry
+    SearchEntry.objects.filter(obj_type='podcast', obj_id=instance.id).delete()
+
+def remove_podcast_group_entry(sender, instance=False, **kwargs):
+    from mygpo.search.models import SearchEntry
+    SearchEntry.objects.filter(obj_type='podcast_group', obj_id=instance.id).delete()
+
 def update_podcast_entry(sender, instance=False, **kwargs):
     from mygpo.search.models import SearchEntry
     # we don't want podcasts in groups to be indexed separately
