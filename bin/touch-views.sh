0dd5460b628cbf25ab728035adaa79c6246b437e
Stefan Kögl <stefan@skoegl.net>
2011-03-09 17:13:37 +0200

    django_couchdb_utils view in touch-views, nice output

--- bin/touch-views.sh
+++ bin/touch-views.sh
@@ -3,6 +3,18 @@
 DIR=`dirname $0`
 DB=`$DIR/../mygpo/print-couchdb.py`
 
-curl -s -X GET ${DB}/_design/core/_view/podcasts_by_id?limit=0
-curl -s -X GET ${DB}/_design/directory/_view/toplist?limit=0
-curl -s -X GET ${DB}/_design/users/_view/users_by_oldid?limit=0
+echo -n "Touching View core ...                 "
+curl -s -X GET ${DB}/_design/core/_view/podcasts_by_id?limit=0 > /dev/null
+echo done
+
+echo -n "Touching View directory ...            "
+curl -s -X GET ${DB}/_design/directory/_view/toplist?limit=0 > /dev/null
+echo done
+
+echo -n "Touching View users ...                "
+curl -s -X GET ${DB}/_design/users/_view/users_by_oldid?limit=0 > /dev/null
+echo done
+
+echo -n "Touching View django_couchdb_utils ... "
+curl -s -X GET ${DB}/_design/django_couchdb_utils/_view/emails?limit=0 > /dev/null
+echo done
