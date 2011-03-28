204e1dfd9fa7d227adda537f29736b92ca9a22af
Stefan Kögl <stefan@skoegl.net>
2011-03-28 08:47:19 +0000

    basic error handling in search backend

--- mygpo/directory/search.py
+++ mygpo/directory/search.py
@@ -12,6 +12,15 @@ def search_wrapper(result):
 
 def search_podcasts(q, limit=20, skip=0):
     db = Podcast.get_db()
+
+    #FIXME current couchdbkit can't parse responses for multi-query searches
+    q = q.replace(',', '')
+
     res = db.search('directory/search', wrapper=search_wrapper,
         include_docs=True, limit=limit, skip=skip, q=q)
-    return list(res), res.total_rows
+
+    #FIXME: return empty results in case of search backend error
+    try:
+        return list(res), res.total_rows
+    except:
+        return [], 0
