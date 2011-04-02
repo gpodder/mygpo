998d95e3eca7a1e6815d41e4f574caeb8b8fe32a
Stefan Kögl <stefan@skoegl.net>
2011-04-02 20:09:16 +0300

    add template tags for linking to podcast pages
    template tag "podcast_group_link" automatically looks for best way to link to a
    podcast (currently always oldid, soon slug, CouchDB-id, etc) and builds a
    link-string for it. PodcastGroups are automatically detected and link to their
    contained podcasts.

--- mygpo/web/templatetags/podcasts.py
+++ mygpo/web/templatetags/podcasts.py
@@ -1,12 +1,13 @@
 from django import template
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
+from django.utils.html import strip_tags
+
 import hashlib
 
 from mygpo.constants import PODCAST_LOGO_SIZE, PODCAST_LOGO_BIG_SIZE
 
 register = template.Library()
-
 def create_podcast_logo(podcast, size):
     size = int(size)
     s = '<img src="%s" alt="%s" height="%d" width="%d" />' % (podcast.get_logo_url(size), _('Logo'), size, size)
@@ -35,3 +36,58 @@ def is_podcast(podcast):
     """ Returns True if the argument is a podcast (esp not a PodcastGroup) """
     from mygpo.core.models import Podcast
     return isinstance(podcast, Podcast)
+
+
+@register.simple_tag
+def podcast_link_target(podcast):
+    """ Returns the link-target for a Podcast, preferring slugs over Ids
+
+    automatically distringuishes between relational Podcast objects and
+    CouchDB-based Podcasts """
+
+    from mygpo.api.models import Podcast as OldPodcast
+    from mygpo.core.models import Podcast
+    from django.core.urlresolvers import reverse
+
+    if isinstance(podcast, OldPodcast):
+        target = podcast.id
+    # we can check for slugs, CouchDB-Ids here, too
+    else:
+        target = podcast.oldid
+
+    return strip_tags(reverse('podcast', args=[target]))
+
+
+@register.simple_tag
+def podcast_group_link(podcast):
+    """ Returns the link strings for Podcast and PodcastGroup objects
+
+    automatically distringuishes between relational Podcast/PodcastGroup
+    objects and CouchDB-based Podcast/PodcastGroup objects """
+
+    from mygpo.api.models import PodcastGroup as OldPodcastGroup
+    from mygpo.core.models import PodcastGroup
+
+    if isinstance(podcast, OldPodcastGroup):
+        podcasts = podcast.podcasts()
+    elif isinstance(podcast, PodcastGroup):
+        podcasts = list(podcast.podcasts)
+    else:
+        return podcast_link(podcast)
+
+    links = (podcast_link(p, p.group_member_name) for p in podcasts)
+    link_text = ' '.join(links)
+    return '%(title)s (%(links)s)' % dict(title=podcast.title, links=link_text)
+
+
+@register.simple_tag
+def podcast_link(podcast, title=None):
+    """ Returns the link for a single Podcast """
+
+    title = title or podcast.group_member_name or \
+        getattr(podcast, 'display_title', None) or podcast.title
+
+    title = strip_tags(title)
+
+    return '<a href="%(target)s" title="%(title)s">%(title)s</a>' % \
+        dict(target=podcast_link_target(podcast), title=title)
