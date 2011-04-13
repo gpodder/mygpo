cdc0ab995c0ff97e97f86f9780c6de6e1cf3ecc8
Stefan Kögl <stefan@skoegl.net>
2011-04-13 11:54:00 +0300

    avoid multiple queries for episode toplist

--- mygpo/directory/_design/views/episode_toplist/map.js
+++ mygpo/directory/_design/views/episode_toplist/map.js
@@ -1,11 +1,36 @@
 function(doc)
 {
+    var toplist_period = 7;
+
     if(doc.doc_type == "Episode")
     {
-        if(doc.listeners > 0 && doc.released != null)
+        if(doc.listeners <= 0 || doc.released == null)
         {
-            day = doc.released.slice(0, 10);
-            emit([day, doc.listeners], null);
+            return;
+        }
+
+        dateString = doc.released.slice(0, 10);
+        dateParts = dateString.split("-");
+        dateParts = dateParts.map(Number)
+
+        for(n=0; n<toplist_period; n++)
+        {
+            dateObj = new Date(dateParts[0], dateParts[1]-1, dateParts[2]+n);
+            year = dateObj.getFullYear();
+            month = Number(dateObj.getMonth()) + 1;
+            if(month < 10)
+            {
+                month = "0" + month;
+            }
+            day = Number(dateObj.getDate());
+            if(day < 10)
+            {
+                day = "0" + day;
+            }
+
+            dateStr = year + "-" + month + "-" + day;
+
+            emit([dateStr, doc.listeners], null);
         }
     }
 }
--- mygpo/directory/toplist.py
+++ mygpo/directory/toplist.py
@@ -7,13 +7,12 @@ from mygpo.utils import daterange
 class EpisodeToplist(object):
     """ Retrieves the episode toplist for a certain time period """
 
-    def __init__(self, timespan=timedelta(days=5), startdate=None):
+    def __init__(self, startdate=None):
         """ Set the covered period of the toplist
 
         The covered period starts at startdate and reaches back for
         the time given by timespan """
 
-        self.timespan = timespan
         self.date = startdate or date.today()
 
 
@@ -22,23 +21,18 @@ class EpisodeToplist(object):
 
         The Episodes are sorted in descreasing order of listeners """
 
-        episodes = []
-        for date in daterange(self.date - self.timespan, self.date):
-            date_str = date.strftime('%Y-%m-%d')
+        date_str = self.date.strftime('%Y-%m-%d')
 
-            r = Episode.view('directory/episode_toplist',
-                    startkey     = [date_str, {}],
-                    endkey       = [date_str, None],
-                    include_docs = True,
-                    descending   = True,
-                    limit        = limit,
-                    skip         = skip,
-                )
-            episodes.extend(list(r))
-            episodes.sort(key=lambda x: x.listeners, reverse=True)
-            episodes = episodes[:100]
+        r = Episode.view('directory/episode_toplist',
+                startkey     = [date_str, {}],
+                endkey       = [date_str, None],
+                include_docs = True,
+                descending   = True,
+                limit        = limit,
+                skip         = skip,
+            )
 
-        return episodes
+        return list(r)
 
 
     def __getitem__(self, key):
