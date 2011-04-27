5ba3f32645fece4ec89823e66cb7957ae35ea518
Stefan Kögl <stefan@skoegl.net>
2011-04-27 16:32:22 +0300

    subscriber_count wasn't updated properly

--- mygpo/directory/management/commands/update-toplist.py
+++ mygpo/directory/management/commands/update-toplist.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 from couchdbkit import ResourceConflict
 
 from mygpo.core.models import Podcast, SubscriberData
+from mygpo.users.models import PodcastUserState
 from mygpo.utils import progress, multi_request_view
 from mygpo.decorators import repeat_on_conflict
 
@@ -14,19 +15,30 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         started = datetime.now()
-        entries = multi_request_view(Podcast, 'core/podcasts_by_oldid')
+        entries = multi_request_view(Podcast, 'core/podcasts_by_oldid', include_docs=True)
         total = Podcast.view('core/podcasts_by_oldid', limit=0).total_rows
 
         for n, entry in enumerate(entries):
-            self.update(entry=entry, started=started)
+            subscriber_count = self.get_subscriber_count(entry.get_id())
+            self.update(entry=entry, started=started, subscriber_count=subscriber_count)
             progress(n, total)
 
 
     @repeat_on_conflict(['entry'])
-    def update(self, entry, started):
+    def update(self, entry, started, subscriber_count):
         data = SubscriberData(
             timestamp        = started,
-            subscriber_count = max(0, entry.subscriber_count()),
+            subscriber_count = max(0, subscriber_count),
             )
         entry.subscribers.append(data)
         entry.save()
+
+
+    @staticmethod
+    def get_subscriber_count(podcast_id):
+        db = PodcastUserState.get_db()
+        x = db.view('users/subscriptions_by_podcast',
+                startkey = [podcast_id, None],
+                endkey   = [podcast_id, {}],
+            )
+        return x.count()
