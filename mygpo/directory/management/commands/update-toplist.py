81b2519f575d35d2f1b735bcaef1901539ee06fa
Stefan Kögl <stefan@skoegl.net>
2011-06-28 13:10:43 +0200

    refactor mgmt cmd update-toplist to use just CouchDB

--- mygpo/directory/management/commands/update-toplist.py
+++ mygpo/directory/management/commands/update-toplist.py
@@ -2,11 +2,9 @@ from datetime import datetime
 
 from django.core.management.base import BaseCommand
 
-from couchdbkit import ResourceConflict
-
 from mygpo.core.models import Podcast, SubscriberData
 from mygpo.users.models import PodcastUserState
-from mygpo.utils import progress, multi_request_view
+from mygpo.utils import progress
 from mygpo.decorators import repeat_on_conflict
 
 
@@ -14,31 +12,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        started = datetime.now()
-        entries = multi_request_view(Podcast, 'core/podcasts_by_oldid', include_docs=True)
+        # couchdbkit doesn't preserve microseconds
+        started = datetime.utcnow().replace(microsecond=0)
+
+        podcasts = Podcast.all_podcasts()
         total = Podcast.view('core/podcasts_by_oldid', limit=0).total_rows
 
-        for n, entry in enumerate(entries):
-            subscriber_count = self.get_subscriber_count(entry.get_id())
-            self.update(entry=entry, started=started, subscriber_count=subscriber_count)
+        for n, podcast in enumerate(podcasts):
+            subscriber_count = self.get_subscriber_count(podcast.get_id())
+            self.update(podcast=podcast, started=started, subscriber_count=subscriber_count)
             progress(n, total)
 
 
-    @repeat_on_conflict(['entry'])
-    def update(self, entry, started, subscriber_count):
+    @repeat_on_conflict(['podcast'])
+    def update(self, podcast, started, subscriber_count):
+
+        # We've already updated this podcast
+        if started in [e.timestamp for e in podcast.subscribers]:
+            return
+
         data = SubscriberData(
             timestamp        = started,
             subscriber_count = max(0, subscriber_count),
             )
-        entry.subscribers.append(data)
-        entry.save()
+
+        podcast.subscribers = sorted(podcast.subscribers + [data], key=lambda e: e.timestamp)
+        podcast.save()
 
 
     @staticmethod
     def get_subscriber_count(podcast_id):
         db = PodcastUserState.get_db()
         x = db.view('users/subscriptions_by_podcast',
-                startkey = [podcast_id, None],
-                endkey   = [podcast_id, {}],
+                startkey    = [podcast_id, None],
+                endkey      = [podcast_id, {}],
+                reduce      = True,
+                group       = True,
+                group_level = 2,
             )
         return x.count()
