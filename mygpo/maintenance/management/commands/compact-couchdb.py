8ac391837e5c39b73e8e6dfd4d86277b3767c886
Stefan Kögl <stefan@skoegl.net>
2011-07-28 16:07:05 +0000

    refactor compact-couchdb
    * views are only compacted once per index file
    * prints duration of compaction
    * prints before/after size of index files

--- mygpo/maintenance/management/commands/compact-couchdb.py
+++ mygpo/maintenance/management/commands/compact-couchdb.py
@@ -17,46 +17,60 @@ class Command(BaseCommand):
     Compacts the database and all views, and measures the required time
     """
 
-    @staticmethod
-    def get_design_docs(db):
-        prefix = '_design/'
-        prefix_len = len(prefix)
-
-        for ddoc in db.view('_all_docs', startkey='_design/', endkey='_design0'):
-            yield ddoc['key'][prefix_len:]
-
-
     def handle(self, *args, **options):
-
         db_url = settings.COUCHDB_DATABASES[0][1]
         db = Database(db_url)
 
-        print 'Compacting Database ... ',
-        sys.stdout.flush()
-        compact_db = lambda: db.compact()
+        for name, compact, is_compacting, get_size in self.get_compacters(db):
+            duration, size_before, size_after = self.compact_wait(compact, is_compacting, get_size)
+            print '%-30s %17s %10s %10s' % (name, duration, self.prettySize(size_before), self.prettySize(size_after))
+
+
+    def get_compacters(self, db):
+        """ Returns tuples containing compaction tasks """
+
+        compact_db       = lambda: db.compact()
         db_is_compacting = lambda: db.info()['compact_running']
-        duration = self.compact_wait(compact_db, db_is_compacting)
-        print duration
+        get_db_size      = lambda: db.info()['disk_size']
 
-        # Only compact once if multiple views share one index file
-        ddocs = {}
-        for ddoc in self.get_design_docs(db):
-            sig = db.res.get('/_design/%s/_info' % ddoc).json_body['view_index']['signature']
-            ddocs[sig] = ddoc
+        yield ('database', compact_db, db_is_compacting, get_db_size)
 
-        for design_doc in ddocs.values():
-            print 'Compacting %s ...' % design_doc,
-            sys.stdout.flush
-            compact_view = lambda: db.compact('%s' % design_doc)
+        for design_doc in self.get_design_docs(db):
+            compact_view       = lambda: db.compact('%s' % design_doc)
             view_is_compacting = lambda: db.res.get('/_design/%s/_info' % design_doc).json_body['view_index']['compact_running']
-            duration = self.compact_wait(compact_view, view_is_compacting)
-            print duration
+            get_view_size      = lambda: db.res.get('/_design/%s/_info' % design_doc).json_body['view_index']['disk_size']
+            yield (design_doc, compact_view, view_is_compacting, get_view_size)
 
 
     @staticmethod
-    def compact_wait(compact, is_compacting, sleep_time=1, inc_factor = 2):
+    def get_all_design_docs(db):
+        """ Returns all design documents in the database """
+
+        prefix = '_design/'
+        prefix_len = len(prefix)
+        return (ddoc['key'][prefix_len:] for ddoc in db.view('_all_docs', startkey='_design/', endkey='_design0'))
+
+
+    def get_design_docs(self, db):
+        """
+        Return one design doc for each index file
+        """
+        ddocs = {}
+        for ddoc in self.get_all_design_docs(db):
+            sig = db.res.get('/_design/%s/_info' % ddoc).json_body['view_index']['signature']
+            ddocs[sig] = ddoc
+
+        return ddocs.values()
+
+
+    @staticmethod
+    def compact_wait(compact, is_compacting, get_size, sleep_time=300, inc_factor = 1):
+        """ Compacts the view and waits for the compaction to finish
+
+        Reports elapsed time and the view size, before and after the compaction """
 
         start = datetime.utcnow()
+        size_before = get_size()
 
         while True:
             try:
@@ -80,5 +94,17 @@ class Command(BaseCommand):
                 print >> sys.stderr, e
 
         end = datetime.utcnow()
+        size_after = get_size()
 
-        return end - start
+        return end - start, size_before, size_after
+
+
+    @staticmethod
+    def prettySize(size):
+        # http://snippets.dzone.com/posts/show/5434
+        suffixes = [("B",2**10), ("K",2**20), ("M",2**30), ("G",2**40), ("T",2**50)]
+        for suf, lim in suffixes:
+            if size > lim:
+                continue
+            else:
+                return round(size/float(lim/2**10),2).__str__()+suf
