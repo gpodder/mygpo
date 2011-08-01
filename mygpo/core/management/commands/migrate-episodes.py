ecf4f62621cda801b9e01250fb854d2060dd381e
Stefan Kögl <stefan@skoegl.net>
2011-08-01 08:19:30 +0000

    bugfixing management command migrate-episodes

--- mygpo/core/management/commands/migrate-episodes.py
+++ mygpo/core/management/commands/migrate-episodes.py
@@ -12,14 +12,17 @@ class Command(BaseCommand):
 
 
     option_list = BaseCommand.option_list + (
-        make_option('--min-id', action='store', type="int", dest='min_id', default=0, help="Id from which the migration should start."),
-        make_option('--max-id', action='store', type="int", dest='max_id', help="Id at which the migration should end."),
+        make_option('--min-id', action='store', type="int", dest='min_id',
+            default=0, help="Id from which the migration should start."),
+
+        make_option('--max-id', action='store', type="int", dest='max_id',
+            default=None,  help="Id at which the migration should end."),
     )
 
     def handle(self, *args, **options):
 
         min_id = options.get('min_id', 0)
-        max_id = options.get('max_id', oldmodels.Episode.objects.order_by('-id')[0].id)
+        max_id = options.get('max_id') or oldmodels.Episode.objects.order_by('-id')[0].id
 
         updated, deleted, created = 0, 0, 0
 
@@ -28,19 +31,21 @@ class Command(BaseCommand):
         total = oldepisodes.count()
         compare = lambda o, n: cmp(long(o.id), long(n.oldid))
 
-        for n, (olde, newe) in enumerate(iterate_together(oldepisodes, newepisodes, compare)):
+        for n, (olde, newe) in enumerate(iterate_together(oldepisodes.iterator(), newepisodes, compare)):
 
             if (olde != None) and (newe != None):
-                podcast = newmodels.Podcast.get(newe.podcast)
                 updated += migrate.update_episode(olde, newe)
 
             elif olde == None:
                 deleted += 1
-                newe.delete()
+                #newe.delete()
 
             elif newe == None:
-                newe = migrate.create_episode(olde)
-                created += 1
+                try:
+                    newe = migrate.create_episode(olde)
+                    created += 1
+                except oldmodels.Podcast.DoesNotExist:
+                    pass
 
             status_str = '%d new, %d upd, %d del' % (created, updated, deleted)
             progress(n, total, status_str)
