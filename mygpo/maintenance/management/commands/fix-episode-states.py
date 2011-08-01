0d3db88763939a5c098fa9450b1befdeaf4bbf16
Stefan Kögl <stefan@skoegl.net>
2011-08-01 10:16:31 +0200

    refactoring fix-episode-states

--- mygpo/maintenance/management/commands/fix-episode-states.py
+++ mygpo/maintenance/management/commands/fix-episode-states.py
@@ -19,25 +19,44 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         states = EpisodeUserState.view('maintenance/incorrect_episode_ids',
+                limit=100,
                 include_docs=True)
 
         total = states.total_rows
+        moved, merged = 0, 0
 
-        for n, state in enumerate(states):
-                user = User.objects.get(id=state.user_oldid)
-                old_podcast = Podcast.objects.get(url=state.podcast_ref_url)
-                old_episode = Episode.objects.get(podcast=old_podcast, url=state.ref_url)
+        for n, state in enumerate(states.iterator()):
+                try:
+                    user = User.objects.get(id=state.user_oldid)
+                except User.DoesNotExist:
+                    state.delete()
+                    continue
+
+                try:
+                    old_podcast = Podcast.objects.get(url=state.podcast_ref_url)
+                except Podcast.DoesNotExist:
+                    state.delete()
+                    continue
+
+                try:
+                    old_episode = Episode.objects.get(podcast=old_podcast, url=state.ref_url)
+                except Episode.DoesNotExist:
+                    state.delete()
+                    continue
+
                 episode = migrate.get_or_migrate_episode(old_episode)
                 new_state = episode.get_user_state(user)
 
                 if not new_state._id:
                     self.update_episode(state=state, episode_id=episode._id)
+                    moved += 1
 
                 else:
                     self.delete(state=state)
-                    self.merge(new_state=state, other_state=state)
+                    self.merge(new_state=new_state, other_state=state)
+                    merged += 1
 
-                progress(n+1, total)
+                progress(n+1, total, 'moved: %d, merged: %d' % (moved, merged))
 
 
     @repeat_on_conflict(['state'])
@@ -48,7 +67,7 @@ class Command(BaseCommand):
 
     @repeat_on_conflict(['new_state'])
     def merge(self, new_state, other_state):
-        merge_episode_states(new_state, state)
+        merge_episode_states(new_state, other_state)
         new_state.save()
 
     @repeat_on_conflict(['state'])
