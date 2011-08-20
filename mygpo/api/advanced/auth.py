b5df6c4416dcbc68ee3c6a6afa52223762185e51
Stefan Kögl <stefan@skoegl.net>
2011-08-20 23:06:14 +0200

    update Authentication API
    http://wiki.gpodder.org/wiki/Web_Services/API/Drafts#Authentication_API

--- mygpo/api/advanced/auth.py
+++ mygpo/api/advanced/auth.py
@@ -18,10 +18,6 @@
 from mygpo.api.basic_auth import require_valid_user, check_username
 from django.contrib import auth
 from django.http import HttpResponse
-from mygpo.api.httpresponse import JsonResponse
-from django.shortcuts import get_object_or_404
-from mygpo.api.models import Device
-from django.utils.translation import ugettext as _
 from datetime import datetime, timedelta
 from django.views.decorators.csrf import csrf_exempt
 
@@ -29,50 +25,21 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 @require_valid_user
 @check_username
-def login(request, username, device_uid):
+def login(request, username):
     """
     authenticates the user with regular http basic auth
-    the device is created if it doesn't already exist
     """
 
-    d, created = Device.objects.get_or_create(user=request.user, uid=device_uid)
-
-    request.session['device'] = device_uid
-    request.session.set_expiry(datetime.now()+timedelta(days=365))
-
-    # the user has been logged in at this point already
-    r = {'valid': True}
-    return JsonResponse(r)
-
-
-@csrf_exempt
-@check_username
-def logout(request, username, device_uid):
-    """
-    logs out the user. does nothing if he wasn't logged in
-    """
-    auth.logout(request)
-
+    request.session.set_expiry(datetime.utcnow()+timedelta(days=365))
     return HttpResponse()
 
 
 @csrf_exempt
-def validate(request, username, device_uid):
+@check_username
+def logout(request, username):
     """
-    checks if the client has been authenticated for the given useru
+    logs out the user. does nothing if he wasn't logged in
     """
-    if not request.user.is_authenticated():
-        return JsonResponse({'valid': False, 'reason': 'Client not authenticated'})
 
-    if request.user.username != username:
-        return JsonResponse({'valid': False, 'reason': 'Client authenticated for different username: %s' % request.user.username})
-
-    get_object_or_404(Device, user=request.user, uid=device_uid)
-
-    # skip if client isn't authenticated for any device
-    if request.session['device'] and (device_uid != request.session['device']):
-        return JsonResponse({'valid': False, 'reason': 'Client authenticated for different device: %s' % request.session['device']})
-
-    return JsonResponse({'valid': True})
-
-
+    auth.logout(request)
+    return HttpResponse()
