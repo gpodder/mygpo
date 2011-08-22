43872d3709f30664aa2e8d238668cb7024616744
Stefan Kögl <stefan@skoegl.net>
2011-08-22 16:25:31 +0000

    add workaround for bug in embed_youtube_video

--- mygpo/web/templatetags/youtube.py
+++ mygpo/web/templatetags/youtube.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
+from mygpo.core.models import Podcast
 from mygpo.data import youtube
 
 register = template.Library()
@@ -14,9 +15,10 @@ def get_youtube_id(url):
 
 @register.filter
 def embed_youtube_video(episode, user):
-
+    #TODO: refactor to template tag and pass podcast as parameter
+    podcast = Podcast.get(episode.podcast)
     s = """embedYoutubeVideo("%s", "%s", "%s", "%s");""" % \
-         (youtube.get_youtube_id(episode.url), user.username, episode.podcast.url, episode.url)
+         (youtube.get_youtube_id(episode.url), user.username, podcast.url, episode.url)
 
     return mark_safe(s)
 
