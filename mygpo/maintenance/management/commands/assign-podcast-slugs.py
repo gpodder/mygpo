dcaa3c888a2c0c338491ac0215938656c15ae642
Stefan Kögl <stefan@skoegl.net>
2011-08-29 13:19:11 +0000

    remove episode-related code from assign-podcast-slugs

--- mygpo/maintenance/management/commands/assign-podcast-slugs.py
+++ mygpo/maintenance/management/commands/assign-podcast-slugs.py
@@ -3,12 +3,11 @@ from itertools import takewhile
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from mygpo.core.models import Podcast
-from mygpo.core.slugs import PodcastSlug, EpisodeSlug, PodcastGroupSlug, \
-         PodcastsMissingSlugs, EpisodesMissingSlugs, \
+from mygpo.core.slugs import PodcastSlug, PodcastGroupSlug, \
+         PodcastsMissingSlugs,
          PodcastGroupsMissingSlugs, assign_slug
 from mygpo.decorators import repeat_on_conflict
-from mygpo.utils import progress, additional_value
+from mygpo.utils import progress
 
 
 class Command(BaseCommand):
@@ -31,29 +30,7 @@ class Command(BaseCommand):
         total = len(podcasts)
 
         for n, podcast in enumerate(takewhile(enough_subscribers, podcasts)):
-            assign_podcast_slug(podcast)
-            progress(n+1, total)
-
-
-        podcast_changed = lambda e, p: e.podcast != pt.get_id()
-        get_podcast = lambda e: Podcast.get(e.podcast)
-
-        episodes = EpisodesMissingSlugs()
-        total = len(episodes)
-        podcast = None
-        common_title = None
-
-        episodes = additional_value(episodes, get_podcast, podcast_changed)
-
-        for n, (episode, podcast) in enumerate(episodes):
-
-            if not podcast.slug:
-                continue
-
-            slug = EpisodeSlug(episode, common_title).get_slug()
-            if slug:
-                self.update_obj(obj=episode, slug=slug)
-
+            assign_slug(podcast, PodcastSlug)
             progress(n+1, total)
 
 
