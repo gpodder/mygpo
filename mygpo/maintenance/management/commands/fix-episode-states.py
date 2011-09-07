8a672e424d562203b4179c582d9d4f922506eb4e
Stefan Kögl <stefan@skoegl.net>
2011-09-07 15:02:46 +0000

    fix-episode-states corrects outdated refs to objs
    When podcasts and episodes are merged, the ids of the merged objects are
    removed; this command checks all episode-state objects and fixes broken
    references

--- mygpo/maintenance/management/commands/fix-episode-states.py
+++ mygpo/maintenance/management/commands/fix-episode-states.py
@@ -1,63 +1,94 @@
-import sys
+from optparse import make_option
+
+from couchdbkit import Consumer
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-from mygpo.core.models import Episode
+from mygpo.core.models import Episode, Podcast
 from mygpo.decorators import repeat_on_conflict
 from mygpo.users.models import EpisodeUserState
 from mygpo.maintenance.merge import merge_episode_states
-from mygpo.utils import progress
+from mygpo.utils import progress, multi_request_view
 from mygpo import migrate
 
+try:
+    from collections import Counter
+except ImportError:
+    from mygpo.counter import Counter
 
 
 class Command(BaseCommand):
-    """
-    """
+    """ Fixes broken references in episode state objects """
+
+
+    option_list = BaseCommand.option_list + (
+            make_option('--since', action='store', type=int, dest='since',
+            default=0, help="Where to start the operation"),
+        )
+
 
     def handle(self, *args, **options):
 
-        states = EpisodeUserState.view('maintenance/incorrect_episode_ids',
-                limit=100,
-                include_docs=True)
+        db = EpisodeUserState.get_db()
+        since = options.get('since')
+        total = db.info()['update_seq']
 
-        total = states.total_rows
-        moved, merged = 0, 0
+        states = self.get_states(db, since)
+        podcasts = {}
+        episodes = {}
+        actions = Counter()
 
-        for n, state in enumerate(states.iterator()):
-                try:
-                    user = User.objects.get(id=state.user_oldid)
-                except User.DoesNotExist:
-                    state.delete()
-                    continue
+        for n, state in states:
 
-                podcast = Podcast.for_url(state.podcast_ref_url)
-                if not podcast:
-                    state.delete()
-                    continue
+            # Podcasts
+            if not state.podcast in podcasts:
 
-                try:
-                    old_episode = Episode.objects.get(podcast=old_podcast, url=state.ref_url)
-                except Episode.DoesNotExist:
-                    state.delete()
-                    continue
+                podcast = Podcast.get(state.podcast)
 
-        for n, state in enumerate(states):
-                user = User.objects.get(id=state.user_oldid)
-                episode = Episode.for_podcast_url(state.podcast_ref_url, state.ref_url)
-                new_state = episode.get_user_state(user)
-
-                if not new_state._id:
-                    self.update_episode(state=state, episode_id=episode._id)
-                    moved += 1
+                if podcast:
+                    podcasts[state.podcast] = True
 
                 else:
-                    self.delete(state=state)
-                    self.merge(new_state=new_state, other_state=state)
-                    merged += 1
+                    if not state.podcast_ref_url:
+                        continue
 
-                progress(n+1, total, 'moved: %d, merged: %d' % (moved, merged))
+                    actions['fetch-podcast'] += 1
+                    podcast = Podcast.for_url(state.podcast_ref_url,create=True)
+                    podcasts[state.podcast] = podcast.get_id()
+
+            if isinstance(podcasts.get(state.podcast, False), basestring):
+                actions['update-podcast'] += 1
+                self.update_podcast(state=state,
+                        podcast_id=podcasts[state.podcast])
+
+
+
+            # Episodes
+            if not state.episode in episodes:
+
+                episode = Episode.get(state.episode)
+
+                if episode:
+                    episodes[state.episode] = True
+
+                else:
+                    if not state.ref_url:
+                        continue
+
+                    actions['fetch-episode'] += 1
+                    episode = Episode.for_podcast_id_url(state.podcast,
+                        state.ref_url, create=True)
+                    episodes[state.episode] = episode._id
+
+            if isinstance(episodes.get(state.episode, False), basestring):
+                actions['update-episode'] += 1
+                self.update_episode(state=state,
+                        episode_id=episodes[state.episode])
+
+
+            status_str = ', '.join('%s: %d' % x for x in actions.items())
+            progress(n, total, status_str)
 
 
     @repeat_on_conflict(['state'])
@@ -65,12 +96,28 @@ class Command(BaseCommand):
         state.episode = episode_id
         state.save()
 
-
-    @repeat_on_conflict(['new_state'])
-    def merge(self, new_state, other_state):
-        merge_episode_states(new_state, other_state)
-        new_state.save()
-
     @repeat_on_conflict(['state'])
-    def delete(self, state):
-        state.delete()
+    def update_podcast(self, state, podcast_id):
+        state.podcast = podcast_id
+        state.save()
+
+
+    @staticmethod
+    def get_states(db, since=0, limit=100):
+        consumer = Consumer(db)
+
+        while True:
+            resp = consumer.wait_once(since=since, limit=limit,
+                    include_docs=True, filter='users/episode_states')
+
+            results = resp['results']
+
+            if not results:
+                break
+
+            for res in results:
+                doc = res['doc']
+                seq = res['seq']
+                yield seq, EpisodeUserState.wrap(doc)
+
+            since = resp['last_seq']
