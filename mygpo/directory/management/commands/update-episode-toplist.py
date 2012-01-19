d2e64b62315312d891798dd8c47a0c3b143dcb7d
Stefan Kögl <stefan@skoegl.net>
2012-01-19 10:08:11 +0100

    make update-episode-toplist a continuous command

--- mygpo/directory/management/commands/update-episode-toplist.py
+++ mygpo/directory/management/commands/update-episode-toplist.py
@@ -1,103 +1,33 @@
-from optparse import make_option
-from datetime import datetime
-
-from django.core.management.base import BaseCommand
-from couchdbkit import Consumer
 from couchdbkit.exceptions import ResourceNotFound
 
 from mygpo.core.models import Episode
 from mygpo.users.models import EpisodeUserState
-from mygpo.utils import progress
 from mygpo.decorators import repeat_on_conflict
-from mygpo.maintenance.models import CommandStatus, CommandRunStatus
+from mygpo.maintenance.management.changescmd import ChangesCommand
 
-try:
-    from collections import Counter
-except ImportError:
-    from mygpo.counter import Counter
+class Command(ChangesCommand):
 
-
-class Command(BaseCommand):
-
-    option_list = BaseCommand.option_list + (
-        make_option('--since', action='store', type=int, dest='since',
-            default=0, help="Where to start the operation"),
-
-        make_option('--continue', action='store_true', dest='continue',
-            default=False, help="Continue from last sequence number"),
-
-        make_option('--silent', action='store_true', dest='silent',
-            default=False, help="Don't display any progress output"),
-    )
+    def __init__(self):
+        super(Command, self).__init__('episode-toplist-status',
+                'Episode-Toplist-Update')
 
 
-    def handle(self, *args, **options):
-        db = EpisodeUserState.get_db()
-        status = self.get_cmd_status()
-        since = self.get_since(status, options)
-        states = self.get_states(db, since)
-        actions = Counter()
+    def handle_obj(self, seq, doc, actions):
+        state = EpisodeUserState.wrap(doc)
 
-        # create unfinished command run status
-        run_status = CommandRunStatus()
-        run_status.timestamp_started = datetime.utcnow()
-        run_status.start_seq = since
-        # add it to existing one (if any)
-        status.runs.append(run_status)
-        status.save()
-
-        total = db.info()['update_seq']
-
-        for seq, state in states:
-            total = db.info()['update_seq']
-
-            try:
-                episode = Episode.get(state.episode)
-            except:
-                episode = None
-
-            if episode:
-                listeners = episode.listener_count()
-                actions['updated'] += self.update(episode=episode, listeners=listeners)
-            else:
-                actions['missing'] += 1
-
-            if not options['silent']:
-                progress(seq, total, ', '.join('%s: %d' % x for x in actions.items()))
-
-
-        # finish command run status
-        run_status.timestamp_finished = datetime.utcnow()
-        run_status.end_seq = total
-        run_status.status_counter = dict(actions)
-        # and overwrite existing one (we could keep a longer log here)
-
-        @repeat_on_conflict(['status'])
-        def _update(status, run_status):
-            status.runs = [run_status]
-            status.save()
-
-        _update(status=status, run_status=run_status)
-
-    @staticmethod
-    def get_cmd_status():
         try:
-            status = CommandStatus.get('episode-toplist-status')
+            episode = Episode.get(state.episode)
+
         except ResourceNotFound:
-            status = CommandStatus()
-            status.command = 'Episode-Toplist-Update'
-            status._id = 'episode-toplist-status'
+            episode = None
 
-        return status
+        if episode:
+            listeners = episode.listener_count()
+            updated = self.update(episode=episode, listeners=listeners)
+            actions['updated'] += updated
 
-
-    @staticmethod
-    def get_since(status, options):
-        if options['continue']:
-            return status.last_seq
         else:
-            return options['since']
-
+            actions['missing'] += 1
 
 
     @repeat_on_conflict(['episode'])
@@ -110,20 +40,9 @@ class Command(BaseCommand):
         return True
 
 
-    @staticmethod
-    def get_states(db, since=0, limit=10, timeout=10):
-        consumer = Consumer(db)
+    def get_query_params(self):
+        return dict(include_docs=True, filter='directory/has_play_events')
 
-        while True:
-            resp = consumer.wait_once(since=since, limit=limit, timeout=timeout, include_docs=True, filter='directory/has_play_events')
-            results = resp['results']
 
-            if not results:
-                break
-
-            for res in results:
-                doc = res['doc']
-                seq = res['seq']
-                yield seq, EpisodeUserState.wrap(doc)
-
-            since = resp['last_seq']
+    def get_db(self):
+        return EpisodeUserState.get_db()
