e4e55564b4a7250715e6fa4ab89607733f2607ad
Stefan Kögl <stefan@skoegl.net>
2012-01-28 11:46:25 +0100

    remove references to Django User

--- mygpo/api/tests.py
+++ mygpo/api/tests.py
@@ -20,7 +20,6 @@ import unittest
 import doctest
 
 from django.test import TestCase
-from django.contrib.auth.models import User
 
 from mygpo.api.opml import Importer, Exporter
 from mygpo.test import create_auth_string
