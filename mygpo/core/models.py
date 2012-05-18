b9361a77b26219155f65d9f26b189f505e2be7f6
Stefan Kögl <stefan@skoegl.net>
2012-05-18 14:40:58 +0200

    fix Podcast.get_all_states()

--- mygpo/core/models.py
+++ mygpo/core/models.py
@@ -698,7 +698,7 @@ class Podcast(Document, SlugMixin, OldIdMixin):
         from mygpo.users.models import PodcastUserState
         return PodcastUserState.view('users/podcast_states_by_podcast',
             startkey = [self.get_id(), None],
-            endkey   = [self.get_id(), '\ufff0'],
+            endkey   = [self.get_id(), {}],
             include_docs=True)
 
     def get_all_subscriber_data(self):
