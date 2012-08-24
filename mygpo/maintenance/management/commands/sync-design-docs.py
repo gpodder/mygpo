78ce96dc537d4db53a356b046bb87231bbd1ad30
Stefan Kögl <stefan@skoegl.net>
2012-08-24 12:19:57 +0000

    split ddocs into "general" and "user"
    Conflicts:
    	mygpo/maintenance/management/commands/sync-design-docs.py

--- mygpo/maintenance/management/commands/sync-design-docs.py
+++ mygpo/maintenance/management/commands/sync-design-docs.py
@@ -13,8 +13,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        path = os.path.join(settings.BASE_DIR, '..', 'couchdb', '_design')
-        db = get_main_database()
-        loader = FileSystemDocsLoader(path)
-        loader.sync(db, verbose=True)
-
+        for part in ('general', 'user'):
+            path = os.path.join(settings.BASE_DIR, '..', 'couchdb', part, '_design')
+            db = Podcast.get_db()
+            db = get_main_database()
+            loader = FileSystemDocsLoader(path)
+            loader.sync(db, verbose=True)
