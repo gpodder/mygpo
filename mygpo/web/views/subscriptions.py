8fefea09c7bd1acfc14d6251b18816ab3afbd475
Stefan Kögl <stefan@skoegl.net>
2012-09-03 20:56:46 +0200

    fix broken subscription changes feed

--- mygpo/web/views/subscriptions.py
+++ mygpo/web/views/subscriptions.py
@@ -118,7 +118,7 @@ def create_subscriptionlist(request):
 @requires_token(token_name='subscriptions_token')
 def subscriptions_feed(request, username):
     # Create to feed manually so we can wrap the token-authentication around it
-    f = SubscriptionsFeed()
+    f = SubscriptionsFeed(username)
     obj = f.get_object(request, username)
     feedgen = f.get_feed(obj, request)
     response = HttpResponse(mimetype=feedgen.mime_type)
@@ -128,6 +128,9 @@ def subscriptions_feed(request, username):
 
 class SubscriptionsFeed(Feed):
     """ A feed showing subscription changes for a certain user """
+
+    def __init__(self, username):
+        self.username = username
 
     def get_object(self, request, username):
         self.site = RequestSite(request)
@@ -169,7 +172,7 @@ class SubscriptionsFeed(Feed):
         else:
             s = _('%(username)s unsubscribed from %(podcast)s (%(site)s)')
 
-        return s % dict(username=entry.user.username,
+        return s % dict(username=self.username,
                         podcast=entry.podcast.display_title,
                         site=self.site)
 
