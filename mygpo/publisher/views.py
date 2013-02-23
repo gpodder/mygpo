de7cd92c5f2f0da45dacafd55728f1e86541beaf
Stefan Kögl <stefan@skoegl.net>
2013-02-23 18:23:26 +0100

    exec publisher-triggered podcast updates w/ celery

--- mygpo/publisher/views.py
+++ mygpo/publisher/views.py
@@ -22,7 +22,7 @@ from mygpo.web.views.podcast import \
          oldid_decorator as podcast_oldid_decorator
 from mygpo.web.utils import get_podcast_link_target, normalize_twitter
 from django.contrib.sites.models import RequestSite
-from mygpo.data.feeddownloader import PodcastUpdater
+from mygpo.data.tasks import update_podcasts
 from mygpo.decorators import requires_token, allowed_methods
 from mygpo.users.models import User
 from mygpo.db.couchdb.episode import episodes_for_podcast
@@ -139,8 +139,8 @@ def update_podcast(request, podcast):
     if not check_publisher_permission(request.user, podcast):
         return HttpResponseForbidden()
 
-    updater = PodcastUpdater()
-    updater.update(podcast.url)
+    update_podcasts.delay([podcast.url])
+    messages.success(request, _('The update has been scheduled. It might take some time until the results are visible.'))
 
     url = get_podcast_link_target(podcast, 'podcast-publisher-detail')
     return HttpResponseRedirect(url)
@@ -175,9 +175,7 @@ def update_published_podcasts(request, username):
         raise Http404
 
     published_podcasts = podcasts_by_id(user.published_objects)
-    updater = PodcastUpdater()
-    updater.update_queue(podcast.url for podcast in published_podcasts)
-
+    update_podcasts.delay([podcast.url for podcast in published_podcasts])
     return HttpResponse('Updated:\n' + '\n'.join([p.url for p in published_podcasts]), mimetype='text/plain')
 
 
