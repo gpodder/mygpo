38eae9aed2f0206bb4603125482dcbb867731956
Stefan Kögl <stefan@skoegl.net>
2013-03-16 14:38:18 +0100

    avoid unicode decode error in caching layer

--- mygpo/cache.py
+++ mygpo/cache.py
@@ -21,7 +21,7 @@ def cache_result(**cache_kwargs):
         @wraps(f)
         def _get(*args, **kwargs):
 
-            key = sha1(str(f.__module__) + str(f.__name__) + str(args) + str(kwargs)).hexdigest()
+            key = sha1(str(f.__module__) + str(f.__name__) + unicode(args) + unicode(kwargs)).hexdigest()
 
             # the timeout parameter can't be used when getting from a cache
             get_kwargs = dict(cache_kwargs)
