9a3b1d8875d199a097e630383723a68f05b91018
Stefan Kögl <stefan@skoegl.net>
2013-07-03 12:45:02 +0200

    set TEMPLATE_CONTEXT_PROCESSORS in a forwards-compatible way
    the defaults may change over time, so they shoudn't be hard-coded in the
    configuration.

--- mygpo/settings.py
+++ mygpo/settings.py
@@ -170,12 +170,10 @@ AUTHENTICATION_BACKENDS = (
 
 SESSION_ENGINE = "django_couchdb_utils.sessions.cached_couchdb"
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.contrib.messages.context_processors.messages",
+
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+
+TEMPLATE_CONTEXT_PROCESSORS += (
     "mygpo.web.google.analytics",
     "mygpo.web.google.adsense",
 )
