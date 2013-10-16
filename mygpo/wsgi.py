304cbbef349b3eea4e949fbf25b5b00daeb2ff8f
Stefan Kögl <stefan@skoegl.net>
2013-10-16 19:56:17 +0000

    move and update wsgi file

--- mygpo/wsgi.py
+++ mygpo/wsgi.py
@@ -18,18 +18,11 @@
 # along with my.gpodder.org. If not, see <http://www.gnu.org/licenses/>.
 
 
-import sys
 import os
 
-import _strptime
-
-# Add this directory as custom Python path
-mygpo_root = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, mygpo_root)
-sys.path.insert(0, os.path.join(mygpo_root, 'lib'))
-
 # Set the DJANGO_SETTINGS_MODULE environment variable
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mygpo.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mygpo.settings")
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
