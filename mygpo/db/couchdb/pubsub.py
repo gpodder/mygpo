5390772e0c9f930693f81b43d64aea26dbf15d89
Stefan Kögl <stefan@skoegl.net>
2013-12-25 13:15:40 +0100

    report unexpected multiple results in pubsub.py

--- mygpo/db/couchdb/pubsub.py
+++ mygpo/db/couchdb/pubsub.py
@@ -1,3 +1,5 @@
+from couchdbkit import MultipleResultsFound
+
 from mygpo.pubsub.models import Subscription
 from mygpo.db.couchdb import get_pubsub_database
 from mygpo.decorators import repeat_on_conflict
@@ -7,8 +9,9 @@ def subscription_for_topic(topic):
     """ return the subscription for the given topic, one None """
 
     db = get_pubsub_database()
+    _view = 'subscriptions/by_topic'
 
-    r = db.view('subscriptions/by_topic',
+    r = db.view(_view,
             key          = topic,
             include_docs = True,
             reduce       = False,
@@ -16,9 +19,24 @@ def subscription_for_topic(topic):
         )
 
     if r:
-        sub = r.one()
+        try:
+            sub = r.one()
+        except MultipleResultsFound as ex:
+            logger.exception('Multiple results found in %s with params %s',
+                             _view, r.params)
+            sub = r.first()
+
         sub.set_db(db)
         return sub
+
+    if r:
+        try:
+            podcast_group = r.one()
+        except MultipleResultsFound as ex:
+            logger.exception('Multiple results found in %s with params %s',
+                             _view, r.params)
+            podcast_group = r.first()
+
 
     else:
         return None
