701e0423f3fc7b6d42eb24196b92d4755f0e7116
Stefan Kögl <stefan@skoegl.net>
2014-02-22 15:26:13 +0100

    missing logger setup

--- mygpo/db/couchdb/pubsub.py
+++ mygpo/db/couchdb/pubsub.py
@@ -4,6 +4,9 @@ from mygpo.pubsub.models import Subscription
 from mygpo.db.couchdb import get_pubsub_database, get_single_result
 from mygpo.decorators import repeat_on_conflict
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 def subscription_for_topic(topic):
     """ return the subscription for the given topic, one None """
