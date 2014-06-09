54de48f1339ab95935e4e356c4da4c27f373345e
Stefan Kögl <stefan@skoegl.net>
2014-06-09 11:20:16 +0200

    update schema migrations

---
+++ mygpo/podcasts/migrations/0002_auto_20140609_0916.py
@@ -0,0 +1,71 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('podcasts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='podcast',
+            name='twitter',
+            field=models.CharField(max_length=15, null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='podcast',
+            name='restrictions',
+            field=models.CharField(max_length=20, null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='podcast',
+            name='episode_count',
+            field=models.PositiveIntegerField(default=0),
+        ),
+        migrations.AlterField(
+            model_name='episode',
+            name='last_update',
+            field=models.DateTimeField(null=True),
+        ),
+        migrations.AlterField(
+            model_name='podcastgroup',
+            name='title',
+            field=models.CharField(db_index=True, max_length=1000, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='podcast',
+            name='last_update',
+            field=models.DateTimeField(null=True),
+        ),
+        migrations.AlterField(
+            model_name='slug',
+            name='slug',
+            field=models.SlugField(max_length=150),
+        ),
+        migrations.AlterField(
+            model_name='episode',
+            name='title',
+            field=models.CharField(db_index=True, max_length=1000, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='episode',
+            name='filesize',
+            field=models.BigIntegerField(null=True),
+        ),
+        migrations.AlterField(
+            model_name='podcast',
+            name='title',
+            field=models.CharField(db_index=True, max_length=1000, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='url',
+            name='url',
+            field=models.URLField(max_length=2048),
+        ),
+    ]
