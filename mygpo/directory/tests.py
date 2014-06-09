fe38ac24250b7f967db6cac236021ceebb799823
Stefan Kögl <stefan@skoegl.net>
2014-06-09 16:20:34 +0200

    add toplist tests

--- mygpo/directory/tests.py
+++ mygpo/directory/tests.py
@@ -17,10 +17,32 @@
 
 import unittest
 import doctest
+import uuid
+from datetime import datetime
 
 from django.test import TestCase
 
+from mygpo.podcasts.models import Podcast
+from mygpo.directory.views import ToplistView
 
-def suite():
-    suite = unittest.TestSuite()
-    return suite
+
+class ToplistTests(unittest.TestCase):
+    """ Test podcast and episode toplists """
+
+    def test_toplist_languages(self):
+        """ Test the all_languages method of the toplists """
+        languages = ['de', 'de_AT', 'en']
+        for lang in languages:
+            Podcast.objects.create(id=uuid.uuid1().hex,
+                                   created=datetime.utcnow(),
+                                   language=lang,
+                                )
+
+        view = ToplistView()
+        all_langs = view.all_languages()
+        self.assertEqual(all_langs, {'de': 'Deutsch', 'en': 'English'})
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTest(unittest.TestLoader().loadTestsFromTestCase(ToplistTests))
+    return tests
