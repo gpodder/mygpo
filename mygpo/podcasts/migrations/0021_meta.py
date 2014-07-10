e762fc5b9ceb1fda0c61bec64a5ec795af66e1f0
Stefan Kögl <stefan@skoegl.net>
2014-07-10 19:27:13 +0200

    [Models] add migration to update Meta options

---
+++ mygpo/podcasts/migrations/0021_meta.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('podcasts', '0020_extend_episode_mimetypes'),
+        ('contenttypes', '__first__'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='episode',
+            options={'ordering': ['-released']},
+        ),
+        migrations.AlterModelOptions(
+            name='mergeduuid',
+            options={'verbose_name': 'Merged UUID', 'verbose_name_plural': 'Merged UUIDs'},
+        ),
+        migrations.AlterModelOptions(
+            name='slug',
+            options={'ordering': ['order']},
+        ),
+        migrations.AlterModelOptions(
+            name='url',
+            options={'ordering': ['order'], 'verbose_name': 'URL', 'verbose_name_plural': 'URLs'},
+        ),
+    ]
