606754f9faadd9805aa1b8e65aef3859cf4bade9
Stefan Kögl <stefan@skoegl.net>
2014-07-10 22:54:31 +0200

    [DB] Fix initial migration of "podcasts" app

--- mygpo/podcasts/migrations/0001_initial.py
+++ mygpo/podcasts/migrations/0001_initial.py
@@ -8,6 +8,7 @@ import uuidfield.fields
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('contenttypes', '__first__'),
     ]
 
     operations = [
@@ -80,37 +81,6 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Episode',
-            fields=[
-                ('id', uuidfield.fields.UUIDField(max_length=32, serialize=False, primary_key=True)),
-                ('title', models.CharField(max_length=1000, blank=True)),
-                ('subtitle', models.CharField(max_length=1000, blank=True)),
-                ('description', models.TextField(blank=True)),
-                ('link', models.URLField(max_length=1000, null=True)),
-                ('language', models.CharField(max_length=10, null=True)),
-                ('last_update', models.DateTimeField()),
-                ('created', models.DateTimeField()),
-                ('modified', models.DateTimeField(auto_now=True)),
-                ('license', models.CharField(max_length=100, null=True)),
-                ('flattr_url', models.URLField(max_length=1000, null=True)),
-                ('content_types', models.CharField(max_length=20, blank=True)),
-                ('outdated', models.BooleanField(default=False)),
-                ('author', models.CharField(max_length=100, null=True, blank=True)),
-                ('guid', models.CharField(max_length=50, null=True)),
-                ('content', models.TextField()),
-                ('released', models.DateTimeField(null=True)),
-                ('duration', models.PositiveIntegerField(null=True)),
-                ('filesize', models.PositiveIntegerField(null=True)),
-                ('mimetypes', models.CharField(max_length=50)),
-                ('listeners', models.PositiveIntegerField(null=True)),
-                ('podcast', models.ForeignKey(to='podcasts.Podcast', to_field=b'id')),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
             name='Podcast',
             fields=[
                 ('id', uuidfield.fields.UUIDField(max_length=32, serialize=False, primary_key=True)),
@@ -136,6 +106,37 @@ class Migration(migrations.Migration):
                 ('episode_count', models.PositiveIntegerField()),
                 ('hub', models.URLField(null=True)),
                 ('related_podcasts', models.ManyToManyField(to=b'podcasts.Podcast')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Episode',
+            fields=[
+                ('id', uuidfield.fields.UUIDField(max_length=32, serialize=False, primary_key=True)),
+                ('title', models.CharField(max_length=1000, blank=True)),
+                ('subtitle', models.CharField(max_length=1000, blank=True)),
+                ('description', models.TextField(blank=True)),
+                ('link', models.URLField(max_length=1000, null=True)),
+                ('language', models.CharField(max_length=10, null=True)),
+                ('last_update', models.DateTimeField()),
+                ('created', models.DateTimeField()),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('license', models.CharField(max_length=100, null=True)),
+                ('flattr_url', models.URLField(max_length=1000, null=True)),
+                ('content_types', models.CharField(max_length=20, blank=True)),
+                ('outdated', models.BooleanField(default=False)),
+                ('author', models.CharField(max_length=100, null=True, blank=True)),
+                ('guid', models.CharField(max_length=50, null=True)),
+                ('content', models.TextField()),
+                ('released', models.DateTimeField(null=True)),
+                ('duration', models.PositiveIntegerField(null=True)),
+                ('filesize', models.PositiveIntegerField(null=True)),
+                ('mimetypes', models.CharField(max_length=50)),
+                ('listeners', models.PositiveIntegerField(null=True)),
+                ('podcast', models.ForeignKey(to='podcasts.Podcast', to_field=b'id')),
             ],
             options={
                 'abstract': False,
