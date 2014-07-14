74032657d3ca1e31c254e6eabaaa40ace42c9983
Stefan Kögl <stefan@skoegl.net>
2014-07-14 18:44:16 +0200

    [Web] Update name of HttpResponse.content_type
    The parameter was previouls called mimetype

--- mygpo/web/views/subscriptions.py
+++ mygpo/web/views/subscriptions.py
@@ -121,7 +121,7 @@ def subscriptions_feed(request, username):
     f = SubscriptionsFeed(username)
     obj = f.get_object(request, username)
     feedgen = f.get_feed(obj, request)
-    response = HttpResponse(mimetype=feedgen.mime_type)
+    response = HttpResponse(content_type=feedgen.mime_type)
     feedgen.write(response, 'utf-8')
     return response
 
