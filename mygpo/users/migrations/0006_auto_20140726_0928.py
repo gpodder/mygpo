ec0d2e0812b45cafda9335e1df9e27b622b19eb6
Stefan Kögl <stefan@skoegl.net>
2014-07-26 11:39:27 +0200

    [Models] update migration files for user models

---
+++ mygpo/users/migrations/0006_auto_20140726_0928.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import mygpo.users.models
+import mygpo.utils
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '__first__'),
+        ('users', '0005_auto_20140719_1105'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProxy',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('auth.user',),
+        ),
+        migrations.AlterField(
+            model_name='client',
+            name='uid',
+            field=models.CharField(max_length=64, validators=[mygpo.users.models.UIDValidator()]),
+        ),
+        migrations.AlterField(
+            model_name='userprofile',
+            name='favorite_feeds_token',
+            field=models.CharField(default=mygpo.utils.random_token, max_length=32, null=True),
+        ),
+        migrations.AlterField(
+            model_name='userprofile',
+            name='publisher_update_token',
+            field=models.CharField(default=mygpo.utils.random_token, max_length=32, null=True),
+        ),
+        migrations.AlterField(
+            model_name='userprofile',
+            name='subscriptions_token',
+            field=models.CharField(default=mygpo.utils.random_token, max_length=32, null=True),
+        ),
+        migrations.AlterField(
+            model_name='userprofile',
+            name='userpage_token',
+            field=models.CharField(default=mygpo.utils.random_token, max_length=32, null=True),
+        ),
+    ]
