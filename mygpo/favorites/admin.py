531fb45a9451a5c3a8d152ec69df2d81f63ffe13
Stefan Kögl <stefan@skoegl.net>
2014-08-19 20:42:42 +0000

    [Admin] add admin for EpisodeHistoryEntry, FavoriteEpisode

--- mygpo/favorites/admin.py
+++ mygpo/favorites/admin.py
@@ -1,3 +1,18 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from mygpo.favorites.models import FavoriteEpisode
+
+
+@admin.register(FavoriteEpisode)
+class FavoriteEpisodeAdmin(admin.ModelAdmin):
+    """ Admin page for favorite episodes """
+
+    # configuration for the list view
+    list_display = ('user', 'episode', )
+
+    # fetch the related objects for the fields in list_display
+    list_select_related = ('user', 'episode', )
+
+    raw_id_fields = ('user', 'episode', )
--- mygpo/history/admin.py
+++ mygpo/history/admin.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from mygpo.history.models import HistoryEntry
+from mygpo.history.models import HistoryEntry, EpisodeHistoryEntry
 
 
 @admin.register(HistoryEntry)
@@ -16,3 +16,15 @@ class HistoryEntryAdmin(admin.ModelAdmin):
     list_select_related = ('user', 'podcast', 'client', )
 
     raw_id_fields = ('user', 'podcast', 'client', )
+
+@admin.register(EpisodeHistoryEntry)
+class EpisodeHistoryEntryAdmin(admin.ModelAdmin):
+    """ Admin page for episode history entries """
+
+    # configuration for the list view
+    list_display = ('user', 'timestamp', 'episode', 'action', 'client', )
+
+    # fetch the related objects for the fields in list_display
+    list_select_related = ('user', 'episode', 'client', )
+
+    raw_id_fields = ('user', 'episode', 'client', )
