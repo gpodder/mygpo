82d3451aceab917c93a12f5192dd0d6a80735fb6
Stefan Kögl <stefan@skoegl.net>
2014-08-22 10:32:28 +0200

    [DB] index User (email, username)

---
+++ mygpo/users/migrations/0008_user_index_email.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0007_syncgroup_protect'),
+        ('auth', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql = 'CREATE INDEX django_auth_user_email ON auth_user (email, username);',
+            reverse_sql = 'DROP INDEX IF EXISTS django_auth_user_email;',
+        )
+    ]
