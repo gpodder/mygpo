90c20cf615fdd226d0e93574e09fe39469729a92
Stefan Kögl <stefan@skoegl.net>
2014-08-26 18:32:03 +0200

    [History] pass callables to URL dispatcher
    using string-based view definitions are deprecated as of Django 1.8
    https://docs.djangoproject.com/en/dev/ref/urls/#url

--- mygpo/history/urls.py
+++ mygpo/history/urls.py
@@ -1,15 +1,18 @@
 from django.conf.urls import url
 
+from mygpo.history.views import (history, history_podcast_id,
+    history_podcast_slug, )
+
 urlpatterns = [
- url(r'^history/$', 'mygpo.history.views.history', name='history'),
+ url(r'^history/$', history, name='history'),
 
  url(r'^podcast/(?P<podcast_id>[0-9a-f]{32})/\+history',
-     'mygpo.history.views.history_podcast_id',
+     history_podcast_id,
      name='podcast-history-id'),
 
  url(r'^podcast/(?P<slug>[\w-]+)/\+history',
-     'mygpo.history.views.history_podcast_slug',
+     history_podcast_slug,
      name='podcast-history-slug'),
 
- url(r'^device/(?P<uid>[\w.-]+)/history$', 'history', name='device-history'),
+ url(r'^device/(?P<uid>[\w.-]+)/history$', history, name='device-history'),
 ]
