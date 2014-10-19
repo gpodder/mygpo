37d9a3255cd3b6509813ecba3676e0b18704d54b
Stefan Kögl <stefan@skoegl.net>
2014-10-19 15:41:13 +0200

    [API] move APIView to mygpo.api

--- mygpo/api/__init__.py
+++ mygpo/api/__init__.py
@@ -14,3 +14,77 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with my.gpodder.org. If not, see <http://www.gnu.org/licenses/>.
 #
+
+from datetime import datetime
+
+from django.http import HttpResponseBadRequest, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
+from django.views.generic.base import View
+
+from mygpo.utils import parse_request_body
+from mygpo.decorators import cors_origin
+from mygpo.users.models import Client
+from mygpo.core.json import JSONDecodeError
+from mygpo.api.basic_auth import require_valid_user, check_username
+
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class RequestException(Exception):
+    """ Raised if the request is malfored or otherwise invalid """
+
+
+class APIView(View):
+
+    @method_decorator(csrf_exempt)
+    @method_decorator(require_valid_user)
+    @method_decorator(check_username)
+    @method_decorator(never_cache)
+    @method_decorator(cors_origin())
+    def dispatch(self, *args, **kwargs):
+        """ Dispatches request and does generic error handling """
+        try:
+            return super(APIView, self).dispatch(*args, **kwargs)
+
+        except Client.DoesNotExist as e:
+            return HttpResponseNotFound(str(e))
+
+        except RequestException as e:
+            return HttpResponseBadRequest(str(e))
+
+    def parsed_body(self, request):
+        """ Returns the object parsed from the JSON request body """
+
+        if not request.body:
+            raise RequestException('POST data must not be empty')
+
+        try:
+            # TODO: implementation of parse_request_body can be moved here
+            # after all views using it have been refactored
+            return parse_request_body(request)
+        except (JSONDecodeError, UnicodeDecodeError, ValueError) as e:
+            msg = u'Could not decode request body for user {}: {}'.format(
+                username, request.body.decode('ascii', errors='replace'))
+            logger.warn(msg, exc_info=True)
+            raise RequestException(msg)
+
+    def get_since(self, request):
+        """ Returns parsed "since" GET parameter """
+        since_ = request.GET.get('since', None)
+
+        if since_ is None:
+            raise RequestException("parameter 'since' missing")
+
+        try:
+            since = datetime.fromtimestamp(int(since_))
+        except ValueError:
+            raise RequestException("'since' is not a valid timestamp")
+
+        if since_ < 0:
+            raise RequestException("'since' must be a non-negative number")
+
+        return since
