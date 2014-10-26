1813cd279dce359110af4b73161e5a5fb94059dc
Stefan Kögl <stefan@skoegl.net>
2014-10-26 11:33:37 +0100

    [Migration] remove unused CouchDB related scripts

--- bin/compact-view.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VIEW=$1
-COUCHDB=`mygpo/print-couchdb.py`
-
-curl -s -H "Content-Type: application/json" -X POST $COUCHDB/_compact/$VIEW > /dev/null
-
-
--- bin/view-usage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# When run on a CouchDB log file, determines the number of
-# requests to individual views
-# Usage: ./view-usage.sh /var/log/couchdb/couch.log
-
-LOGFILE=$1
-
-cat $LOGFILE | grep GET | awk '{print $13;}' | cut -f1 -d"?" | awk '{count[$1]++}END{for(j in count) print count[j],j}' | sort -n -r | head -n 10
