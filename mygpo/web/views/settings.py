0f658a6a6264625ce399d22d514f1c776c5263bc
Stefan Kögl <stefan@skoegl.net>
2014-11-28 09:40:00 +0000

    [Web] add missing import

--- mygpo/web/views/settings.py
+++ mygpo/web/views/settings.py
@@ -23,6 +23,7 @@ from django.contrib import messages
 from django.forms import ValidationError
 from django.utils.translation import ugettext as _
 from django.contrib.auth.decorators import login_required
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.models import RequestSite
 from django.views.decorators.vary import vary_on_cookie
 from django.views.decorators.cache import never_cache, cache_control
