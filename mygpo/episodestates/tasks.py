20d64d6fe340a758e1eae6f1c1f2b0b9bc21998f
Stefan Kögl <stefan@skoegl.net>
2015-06-04 09:15:38 +0000

    [Episodes] pass Id instead of whole obj to update_episode_state

--- mygpo/episodestates/tasks.py
+++ mygpo/episodestates/tasks.py
@@ -1,14 +1,17 @@
 from celery.utils.log import get_task_logger
 
 from mygpo.celery import celery
+from mygpo.history.models import EpisodeHistoryEntry
 from mygpo.episodestates.models import EpisodeState
 
 logger = get_task_logger(__name__)
 
 
 @celery.task
-def update_episode_state(historyentry):
+def update_episode_state(historyentry_pk):
     """ Updates the episode state with the saved EpisodeHistoryEntry """
+
+    historyentry = EpisodeHistoryEntry.objects.get(pk=historyentry_pk)
 
     user = historyentry.user
     episode = historyentry.episode
