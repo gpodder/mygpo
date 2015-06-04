f1f666545fabce96e301960b7ccf843a2fd74c29
Stefan Kögl <stefan@skoegl.net>
2015-06-04 18:25:13 +0200

    [Config] get configuration from environment variables

--- mygpo/settings.py
+++ mygpo/settings.py
@@ -16,26 +16,43 @@
 # along with my.gpodder.org. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import re
 import sys
 import os.path
 import dj_database_url
 
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# http://code.djangoproject.com/wiki/BackwardsIncompatibleChanges#ChangedthewayURLpathsaredetermined
-FORCE_SCRIPT_NAME=""
 
-DEBUG = True
+def get_bool(name, default):
+    return os.getenv(name, str(default)).lower() == 'true'
+
+
+DEBUG = get_bool('DEBUG', False)
+
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = ()
+ADMINS = re.findall(r'\s*([^<]+) <([^>]+)>\s*', os.getenv('ADMINS', ''))
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': dj_database_url.config(
-                default='postgres://mygpo:mygpo@localhost/mygpo'),
+        default='postgres://mygpo:mygpo@localhost/mygpo'),
 }
+
+
+_cache_used = bool(os.getenv('CACHE_BACKEND', False))
+
+if _cache_used:
+    CACHES['default'] = {
+        'BACKEND': os.getenv(
+            'CACHE_BACKEND',
+            'django.core.cache.backends.memcached.MemcachedCache'),
+        'LOCATION': os.getenv('CACHE_LOCATION'),
+    }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -63,14 +80,14 @@ STATICFILES_DIRS = (
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.app_directories.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.locale.LocaleMiddleware',
@@ -121,20 +138,23 @@ try:
     INSTALLED_APPS += ('debug_toolbar', )
 
 except ImportError:
-    print('Could not load django-debug-toolbar', file=sys.stderr)
+    pass
 
 
 try:
     import opbeat
     INSTALLED_APPS += ('opbeat.contrib.django', )
 
+    # add opbeat middleware to the beginning of the middleware classes list
+    MIDDLEWARE_CLASSES = \
+        ('opbeat.contrib.django.middleware.OpbeatAPMMiddleware',) + \
+        MIDDLEWARE_CLASSES
+
 except ImportError:
     pass
 
 
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-ACCOUNT_ACTIVATION_DAYS = 7
+ACCOUNT_ACTIVATION_DAYS = int(os.getenv('ACCOUNT_ACTIVATION_DAYS', 7))
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
@@ -170,22 +190,33 @@ USER_CLASS = 'mygpo.users.models.User'
 
 LOGIN_URL = '/login/'
 
-CSRF_FAILURE_VIEW='mygpo.web.views.security.csrf_failure'
+CSRF_FAILURE_VIEW = 'mygpo.web.views.security.csrf_failure'
 
 
-# The following entries should be set in settings_prod.py
-DEFAULT_FROM_EMAIL = ''
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', '')
+
 SECRET_KEY = os.getenv('SECRET_KEY', '')
-GOOGLE_ANALYTICS_PROPERTY_ID=''
-DIRECTORY_EXCLUDED_TAGS = ()
-FLICKR_API_KEY = ''
 
-MAINTENANCE = os.path.exists(os.path.join(BASE_DIR, 'MAINTENANCE'))
+if 'test' in sys.argv:
+    SECRET_KEY = 'test'
+
+GOOGLE_ANALYTICS_PROPERTY_ID = os.getenv('GOOGLE_ANALYTICS_PROPERTY_ID', '')
+
+DIRECTORY_EXCLUDED_TAGS = os.getenv('DIRECTORY_EXCLUDED_TAGS', '').split()
+
+FLICKR_API_KEY = os.getenv('FLICKR_API_KEY', '')
+
+SOUNDCLOUD_CONSUMER_KEY = os.getenv('SOUNDCLOUD_CONSUMER_KEY', '')
+
+MAINTENANCE = get_bool('MAINTENANCE', False)
+
+
+ALLOWED_HOSTS = ['*']
 
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
             'format': '%(asctime)s %(name)s %(levelname)s %(message)s',
@@ -197,60 +228,86 @@ LOGGING = {
         },
     },
     'handlers': {
-        'console':{
-            'level': 'DEBUG',
+        'console': {
+            'level': os.getenv('LOGGING_CONSOLE_LEVEL', 'DEBUG'),
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
+            'formatter': 'verbose',
         },
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
         },
     },
     'loggers': {
         'django': {
-            'handlers': ['console'],
+            'handlers': os.getenv('LOGGING_DJANGO_HANDLERS',
+                                  'console').split(),
             'propagate': True,
-            'level': 'WARN',
+            'level': os.getenv('LOGGING_DJANGO_LEVEL', 'WARN'),
         },
         'mygpo': {
-            'handlers': ['console'],
-            'level': 'INFO',
+            'handlers': os.getenv('LOGGING_MYGPO_HANDLERS', 'console').split(),
+            'level': os.getenv('LOGGING_MYGPO_LEVEL', 'INFO'),
         },
         'celery': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
+            'handlers': os.getenv('LOGGING_CELERY_HANDLERS',
+                                  'console').split(),
+            'level': os.getenv('LOGGING_CELERY_LEVEL', 'DEBUG'),
         },
     },
 }
 
+_use_log_file = bool(os.getenv('LOGGING_FILENAME', False))
+
+if _use_log_file:
+    LOGGING['handlers']['file'] = {
+        'level': 'INFO',
+        'class': 'logging.handlers.RotatingFileHandler',
+        'filename': os.getenv('LOGGING_FILENAME'),
+        'maxBytes': 10000000,
+        'backupCount': 10,
+        'formatter': 'verbose',
+    }
+
+
 # minimum number of subscribers a podcast must have to be assigned a slug
-PODCAST_SLUG_SUBSCRIBER_LIMIT = 10
+PODCAST_SLUG_SUBSCRIBER_LIMIT = int(os.getenv(
+                                    'PODCAST_SLUG_SUBSCRIBER_LIMIT', 10))
 
 # minimum number of subscribers that a podcast needs to "push" one of its
 # categories to the top
-MIN_SUBSCRIBERS_CATEGORY=10
+MIN_SUBSCRIBERS_CATEGORY = int(os.getenv('MIN_SUBSCRIBERS_CATEGORY', 10))
 
 # maximum number of episode actions that the API processes immediatelly before
 # returning the response. Larger requests will be handled in background.
 # Handler can be set to None to disable
-API_ACTIONS_MAX_NONBG=100
-API_ACTIONS_BG_HANDLER='mygpo.api.tasks.episode_actions_celery_handler'
+API_ACTIONS_MAX_NONBG = int(os.getenv('API_ACTIONS_MAX_NONBG', 100))
+API_ACTIONS_BG_HANDLER = 'mygpo.api.tasks.episode_actions_celery_handler'
 
 
-ADSENSE_CLIENT = ''
-ADSENSE_SLOT_BOTTOM = ''
+ADSENSE_CLIENT = os.getenv('ADSENSE_CLIENT', '')
+
+ADSENSE_SLOT_BOTTOM = os.getenv('ADSENSE_SLOT_BOTTOM', '')
+
+# we're running behind a proxy that sets the X-Forwarded-Proto header correctly
+# see https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # enabled access to staff-only areas with ?staff=<STAFF_TOKEN>
-STAFF_TOKEN = None
+STAFF_TOKEN = os.getenv('STAFF_TOKEN', None)
 
 # Flattr settings -- available after you register your app
-FLATTR_KEY = ''
-FLATTR_SECRET = ''
+FLATTR_KEY = os.getenv('FLATTR_KEY', '')
+FLATTR_SECRET = os.getenv('FLATTR_SECRET', '')
 
-# Flattr thing of the webservice. Will be flattr'd when a user sets the "Auto-Flattr gpodder.net" option
-FLATTR_MYGPO_THING='https://flattr.com/submit/auto?user_id=stefankoegl&url=http://gpodder.net'
+# Flattr thing of the webservice. Will be flattr'd when a user sets
+# the "Auto-Flattr gpodder.net" option
+FLATTR_MYGPO_THING = os.getenv(
+    'FLATTR_MYGPO_THING',
+    'https://flattr.com/submit/auto?user_id=stefankoegl&url=http://gpodder.net'
+)
 
 # The User-Agent string used for outgoing HTTP requests
 USER_AGENT = 'gpodder.net (+https://github.com/gpodder/mygpo)'
@@ -259,25 +316,30 @@ USER_AGENT = 'gpodder.net (+https://github.com/gpodder/mygpo)'
 # available.  Request handlers, for example, can access the requested domain.
 # Code that runs in background can not do this, and therefore requires a
 # default value. This should be set to something like 'http://example.com'
-DEFAULT_BASE_URL = ''
+DEFAULT_BASE_URL = os.getenv('DEFAULT_BASE_URL', '')
 
 
 ### Celery
 
-BROKER_URL='redis://localhost'
-CELERY_RESULT_BACKEND='redis://localhost'
+BROKER_URL = os.getenv('BROKER_URL', 'redis://localhost')
+CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 
-#CELERY_SEND_TASK_ERROR_EMAILS = True
-SERVER_EMAIL = "no-reply@example.com"
+SERVER_EMAIL = os.getenv('SERVER_EMAIL', 'no-reply@example.com')
 
+CELERY_TASK_RESULT_EXPIRES = 60 * 60  # 1h expiry time in seconds
+
+CELERY_ACCEPT_CONTENT = ['pickle', 'json']
+
+CELERY_SEND_TASK_ERROR_EMAILS = get_bool('CELERY_SEND_TASK_ERROR_EMAILS',
+                                         False)
 
 ### Google API
 
-GOOGLE_CLIENT_ID=''
-GOOGLE_CLIENT_SECRET=''
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID', '')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET', '')
 
 # URL where users of the site can get support
-SUPPORT_URL=''
+SUPPORT_URL = os.getenv('SUPPORT_URL', '')
 
 
 FEEDSERVICE_URL = os.getenv('FEEDSERVICE_URL', 'http://feeds.gpodder.net/')
@@ -292,15 +354,15 @@ ELASTICSEARCH_TIMEOUT = float(os.getenv('ELASTICSEARCH_TIMEOUT', '2'))
 # will be deleted
 ACTIVATION_VALID_DAYS = int(os.getenv('ACTIVATION_VALID_DAYS', 10))
 
-import sys
-if 'test' in sys.argv:
-    SECRET_KEY = 'test'
+
+OPBEAT = {
+    "ORGANIZATION_ID": os.getenv('OPBEAT_ORGANIZATION_ID', ''),
+    "APP_ID": os.getenv('OPBEAT_APP_ID', ''),
+    "SECRET_TOKEN": os.getenv('OPBEAT_SECRET_TOKEN', ''),
+}
 
 
 INTERNAL_IPS = os.getenv('INTERNAL_IPS', '').split()
 
-try:
-    from .settings_prod import *
-except ImportError as e:
-    import sys
-    print('create settings_prod.py with your customized settings', file=sys.stderr)
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND',
+                          'django.core.mail.backends.smtp.EmailBackend')
