e8237a7a28cc07a8ac6ac48cfbf934d27a25eede
Stefan Kögl <stefan@skoegl.net>
2015-06-05 21:30:44 +0000

    [Feeds] fix encoding issue with Flickr feeds

--- mygpo/data/flickr.py
+++ mygpo/data/flickr.py
@@ -28,7 +28,7 @@ def get_photo_sizes(photo_id):
     api_key = settings.FLICKR_API_KEY
     request = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=%s&photo_id=%s&format=json' % (api_key, photo_id)
 
-    resp = urllib.request.urlopen(request).read()
+    resp = urllib.request.urlopen(request).read().decode('utf-8')
 
     extract_re = '^jsonFlickrApi\((.*)\)$'
     m = re.match(extract_re, resp)
