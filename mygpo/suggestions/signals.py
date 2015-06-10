e42c1419ddcd603e14dd7fd810ba32d597646c30
Stefan Kögl <stefan@skoegl.net>
2015-06-10 20:18:44 +0200

    [Suggestions] Remove unused module

--- mygpo/suggestions/signals.py
+++
@@ -1,4 +0,0 @@
-from mygpo.subscriptions.signals import subscription_changed
-
-subscription_changed.connect(update_suggestions_on_subscription,
-                             dispatch_uid='update_suggestions_on_subscription')
