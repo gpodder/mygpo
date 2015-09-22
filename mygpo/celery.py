a2a2d5a3a579cf86d813a5d5586c2c3b296dbc72
Stefan Kögl <stefan@skoegl.net>
2015-09-22 20:08:49 +0000

    [Tasks] fix celery startup

--- mygpo/celery.py
+++ mygpo/celery.py
@@ -1,8 +1,10 @@
-
+import os
 
 from django.conf import settings
 
 from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mygpo.settings')
 
 celery = Celery('mygpo.celery')
 celery.config_from_object('django.conf:settings')
