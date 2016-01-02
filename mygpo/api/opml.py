a5b7b44733d6a2063abf3fb415f9358c6411353a
Matt Scheirer <matt.scheirer@gmail.com>
2016-01-02 18:35:58 -0500

    Exporter is not obeying OPML spec - text should be top element title, and is not optional. The optional description field is for... the description. But if text would otherwise be empty, we continue putting the description there.

--- mygpo/api/opml.py
+++ mygpo/api/opml.py
@@ -108,7 +108,8 @@ class Exporter(object):
 
             outline = doc.createElement('outline')
             outline.setAttribute('title', title or '')
-            outline.setAttribute('text', description or '')
+            outline.setAttribute('description', description or '')
+            outline.setAttribute('text', title or description)
             outline.setAttribute('xmlUrl', url)
             outline.setAttribute('type', 'rss')
             return outline
