eab11bd4910f95ea246e611b54c81dc90b08ef46
Matthew Hughes <matthewhughes934@gmail.com>
2019-12-15 18:14:24 +0000

    Fix deprecation warning on 'AssertEquals' in tests
    Fixes warning: "DeprecationWarning: Please use assertEqual instead"

--- mygpo/data/tests.py
+++ mygpo/data/tests.py
@@ -51,7 +51,7 @@ class FlickrTests(TestCase):
 
             sizes = flickr.get_photo_sizes('1235123123')
 
-        self.assertEquals(sizes, API_RESPONSE['sizes']['size'])
+        self.assertEqual(sizes, API_RESPONSE['sizes']['size'])
 
     def test_display_image(self):
         with responses.RequestsMock() as rsps:
@@ -63,4 +63,4 @@ class FlickrTests(TestCase):
                 'https://farm9.staticflickr.com/8747/12346789012_bf1e234567_b.jpg'
             )
 
-        self.assertEquals(disp_photo, MEDIUM_URL)
+        self.assertEqual(disp_photo, MEDIUM_URL)
