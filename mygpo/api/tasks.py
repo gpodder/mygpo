b590e7b6462b31f4f6ebd66fec0f037e8852bf7e
Josh Clements <josh@fndtls.co>
2021-10-11 12:46:14 -0400

    Removing deprecated default_app_config statements

--- mygpo/api/tasks.py
+++ mygpo/api/tasks.py
@@ -1,3 +1,4 @@
+from celery.app import shared_task
 from celery.utils.log import get_task_logger
 
 from django_db_geventpool.utils import close_connection
@@ -8,7 +9,7 @@ from mygpo.api.advanced import update_episodes
 logger = get_task_logger(__name__)
 
 
-@celery.task(max_retries=5, default_retry_delay=60)
+@shared_task(max_retries=5, default_retry_delay=60)
 @close_connection
 def import_episode_actions(user, actions, upload_ts, ua_string):
     logger.info("Importing %d tasks for user %s", len(actions), user)
